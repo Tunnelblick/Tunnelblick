5da61a0798797c0bd5f65be15c71d6db6306d073
angelolaub <angelolaub@ecaf4b9d-c145-0410-a236-1b700048ef5c>
2008-07-17 20:22:03 +0000

    deleted old tunnelblick project

--- trunk/tunnelblick/setpermissions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo chown root:wheel build/Development/Tunnelblick.app/Contents/Resources/openvpnstart
-sudo chmod +s build/Development/Tunnelblick.app/Contents/Resources/openvpnstart
-
-sudo chown -R root:wheel build/Development/Tunnelblick.app/Contents/Resources/*.kext
-
-sudo chown -R root:wheel ./*.kext
