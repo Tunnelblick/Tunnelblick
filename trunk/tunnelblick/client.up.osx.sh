08021d555945394d9bb3affd2b52c8999f93cfa6
angelolaub <angelolaub@ecaf4b9d-c145-0410-a236-1b700048ef5c>
2008-07-25 14:23:45 +0000

    fixed client.up shell script

--- trunk/tunnelblick/client.up.osx.sh
+++ trunk/tunnelblick/client.up.osx.sh
@@ -8,9 +8,8 @@ INTERFACE=$1
 IDENTIFYER="org.openvpn.${INTERFACE}"
 
 # only do something when the server really is pushing something
-# What do we actually expect from foreign_option?
 if [ "$foreign_option_1" == "" ]; then
-	exit 0;
+	exit 0
 fi
 
 nOptionIndex=1
@@ -19,46 +18,27 @@ unset vForOptions
 unset vDNS
 unset vOptions
 
-# This starts as 1 and enters the loop
-while vForOptions = foreign_option_${nOptionIndex};	[ -n "${!vForOptions}" ]; 
-do
+while vForOptions=foreign_option_$nOptionIndex; [ -n "${!vForOptions}" ]; do
 	{
-	
-	# Examples documented for first run
-	# vOptions[0] = foreign_option_1
 	vOptions[nOptionIndex-1]=${!vForOptions}
-
-	# Here we take the value from vOptions[0]
-	# We figure out what it is
 	case ${vOptions[nOptionIndex-1]} in
-		# If it's a search domain
-		*DOMAIN* )
-			# Stuff the contents of the array into $domain
-			# $domain = {CONTENTS OF array} . //dhcp-option DOMAIN /
-			domain=${vOptions[nOptionIndex-1]//dhcp-option DOMAIN /}
-		;;
-		# If it's a DNS server the client will use
-		*DNS*    )
-			# Stuff the contents of the array into $vDNS
-			# vDNS[0] = {CONTENTS OF array} . //dhcp-option DNS /
-			vDNS[nNameServerIndex-1]=${vOptions[nOptionIndex-1]//dhcp-option DNS /}
-			# Increment the name server index to allow for adding more dns servers
-			let nNameServerIndex++
-		;;
-	esac
-
-	let nOptionIndex++
-
-	}
+	*DOMAIN* )
+	domain=${vOptions[nOptionIndex-1]//dhcp-option DOMAIN /}
+	;;
+	*DNS*    )
+	vDNS[nNameServerIndex-1]=${vOptions[nOptionIndex-1]//dhcp-option DNS /}
+	let nNameServerIndex++
+	;;
+esac
+let nOptionIndex++
+}
 done
 
-# Set domain to a default value when no domain is being transmitted
+# set domain to a default value when no domain is being transmitted
 if [ "$domain" -eq "" ]; then
-	# The remote server didn't set a search domain
-	domain="openvpn.automatically.set.the.search.domain.to.something.invalid"
+	domain="openvpn"
 fi
 
-# Oh my lord. Please remove the forks from my subshelled bleeding eyes.
 PSID=$( (scutil | grep PrimaryService | sed -e 's/.*PrimaryService : //')<< EOF
 open
 get State:/Network/Global/IPv4
@@ -91,8 +71,8 @@ quit
 EOF
 )
 
-# Perhaps this should go in /var/tmp?
-# Save old dns settings to temporary file
+
+# save old dns settings to temporary file
 if [ ! -e /tmp/openvpn_dns_${PSID} ]; then
 	echo "$OLDDNS1 $OLDDNS2" > /tmp/openvpn_dns_${PSID}
 fi
