dfa9a988a592ad31fa119649eb7504da9c73c49c
angelolaub <angelolaub@ecaf4b9d-c145-0410-a236-1b700048ef5c>
2008-07-25 01:41:36 +0000

    re-checkin of sparkle II

---
+++ trunk/third_party/Sparkle.framework/Versions/A/Headers/SUUpdateDriver.h
@@ -0,0 +1,29 @@
+//
+//  SUUpdateDriver.h
+//  Sparkle
+//
+//  Created by Andy Matuschak on 5/7/08.
+//  Copyright 2008 Andy Matuschak. All rights reserved.
+//
+
+#ifndef SUUPDATEDRIVER_H
+#define SUUPDATEDRIVER_H
+
+#import <Cocoa/Cocoa.h>
+
+extern NSString *SUUpdateDriverFinishedNotification;
+
+@interface SUUpdateDriver : NSObject
+{
+	BOOL finished;
+	id delegate;
+}
+- (void)checkForUpdatesAtURL:(NSURL *)appcastURL hostBundle:(NSBundle *)hb;
+- (void)abortUpdate;
+- (BOOL)finished;
+
+- delegate;
+- (void)setDelegate:delegate;
+@end
+
+#endif
