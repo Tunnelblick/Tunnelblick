55996c078ee5ad79daae90ae46453d8d7a4ca213
angelolaub <angelolaub@ecaf4b9d-c145-0410-a236-1b700048ef5c>
2008-07-31 10:11:50 +0000

    update license headers

--- tunnelblick/VPNConnection.h
+++ tunnelblick/VPNConnection.h
@@ -1,10 +1,21 @@
-//
-//  VPNConnection.h
-//  Tunnelblick
-//
-//  Created by Angelo Laub on 6/4/05.
-//  Copyright 2005 __MyCompanyName__. All rights reserved.
-//
+/*
+ * Copyright (c) 2004 Angelo Laub
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program (see the file COPYING included with this
+ *  distribution); if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 
 #import <Cocoa/Cocoa.h>
 #import <CoreFoundation/CoreFoundation.h>
--- tunnelblick/installer.m
+++ tunnelblick/installer.m
@@ -1,10 +1,19 @@
 /*
- *  installer.c
- *  HotspotShield
+ *  Copyright (c) 2004 Angelo Laub
  *
- *  Created by Angelo Laub on 7/12/06.
- *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
  *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program (see the file COPYING included with this
+ *  distribution); if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include "installer.h"
