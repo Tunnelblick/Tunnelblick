1ff931432db705438d13abf4a318cfa87c65a9e4
jkbullard <jkbullard@gmail.com>
2009-06-19 13:51:20 +0000

    New up/down scripts to save/restore DNS settings properly (thanks to diego.ri...@rbxglobal.com)

--- tunnelblick/client.down.osx.sh
+++ tunnelblick/client.down.osx.sh
@@ -4,38 +4,24 @@ trap "" HUP
 trap "" INT
 export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
 
-INTERFACE=$1
-IDENTIFYER="org.openvpn.${INTERFACE}"
+# Find the primary service ID and unload the OpenVPN marker
+PSID="$( (scutil | grep Service | sed -e 's/.*Service : //')<<- EOF
+	open
+	show State:/Network/OpenVPN
+	quit
+EOF)"
 
-# if [ "$foreign_option_1" == "" ]; then
-# 	exit 0
-# fi
+# Unload the lease watcher
+LEASE_WATCHER="$(dirname "${0}")/LeaseWatch.plist"
+launchctl unload "${LEASE_WATCHER}"
 
-PSID=$( (scutil | grep PrimaryService | sed -e 's/.*PrimaryService : //')<< EOF
-open
-get State:/Network/Global/IPv4
-d.show
-quit
+# Restore configurations
+scutil <<- EOF
+	open
+	get State:/Network/OpenVPN/OldDNS
+	set State:/Network/Service/${PSID}/DNS
+	remove State:/Network/OpenVPN/OldDNS
+	remove State:/Network/OpenVPN/DNS
+	remove State:/Network/OpenVPN
+	quit
 EOF
-)
-
-if [ ! -e /tmp/openvpn_dns_${PSID} ]; then
-	exit 0
-fi
-if [ ! -e /tmp/openvpn_domain_${PSID} ]; then
-	exit 0
-fi
-
-
-scutil << EOF
-open
-d.init
-d.add ServerAddresses * `cat /tmp/openvpn_dns_${PSID}`
-d.add DomainName `cat /tmp/openvpn_domain_${PSID}`
-set State:/Network/Service/${PSID}/DNS
-quit
-EOF
-
-
-rm /tmp/openvpn_dns_${PSID} /tmp/openvpn_domain_${PSID}
-
