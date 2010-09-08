c2311f1ba7dbb63c217357c10977d31037716fd6
jkbullard@gmail.com <jkbullard@gmail.com@ecaf4b9d-c145-0410-a236-1b700048ef5c>
2010-09-08 21:25:15 +0000

    Build 1011: Fixes Issue 159 problem that crashed that crashed client.down.tunnelblick.sh

--- trunk/tunnelblick/client.down.tunnelblick.sh
+++ trunk/tunnelblick/client.down.tunnelblick.sh
@@ -6,6 +6,11 @@ export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
 
 # NOTE: This script does not use any arguments passed to it by OpenVPN, so it doesn't shift Tunnelblick options out of the argument list
 
+# Do something only if the server pushed something
+if [ "$foreign_option_1" == "" ]; then
+	exit 0
+fi
+
 # Get info saved by the up script
 TUNNELBLICK_CONFIG="$(/usr/sbin/scutil <<-EOF
 	open
@@ -19,12 +24,6 @@ SCRIPT_LOG_FILE="$(echo "${TUNNELBLICK_CONFIG}" | grep -i '^[[:space:]]*ScriptLo
 # Don't need: ARG_RESTORE_ON_DNS_RESET="$(echo "${TUNNELBLICK_CONFIG}" | grep -i '^[[:space:]]*RestoreOnDNSReset :' | sed -e 's/^.*: //g')"
 # Don't need: ARG_RESTORE_ON_WINS_RESET="$(echo "${TUNNELBLICK_CONFIG}" | grep -i '^[[:space:]]*RestoreOnWINSReset :' | sed -e 's/^.*: //g')"
 # Don't need: PROCESS="$(echo "${TUNNELBLICK_CONFIG}" | grep -i '^[[:space:]]*PID :' | sed -e 's/^.*: //g')"
-
-# Do something only if the server pushed something
-if [ "$foreign_option_1" == "" ]; then
-    echo"$(date '+%a %b %e %T %Y') *Tunnelblick client.down.tunnelblick.sh: No network configuration changes were made so none will be restored" >> "${SCRIPT_LOG_FILE}"
-	exit 0
-fi
 
 # Issue warning if the primary service ID has changed
 PSID_CURRENT="$( (scutil | grep Service | sed -e 's/.*Service : //')<<- EOF
