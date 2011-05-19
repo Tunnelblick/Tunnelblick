cb5f41a967f72ddf225cc06983d22c1a55481a54
jkbullard <jkbullard@gmail.com>
2011-05-19 02:51:54 +0000

    Build 2498: Added the ability to have sounds announce connection/disconnection and some enhancements useful for large-scale deployers of Tunnelblick and potentially for VPN service providers:
     * Optionally plays a sound when the connection is made or unexpectedly terminated or restarted. Two per-configuration preferences, 'XXX-tunnelUpSoundName' and 'XXX-tunnelDownSoundName' (where XXX is the name of the configuration) are used. If they are present, the named sound is used. Sounds are searched for in the Tunnelblick.app bundle, then in ~/Library/Sounds, /Library/Sounds, /Network/Library/Sounds, and /System/Library/Sounds, in that order. If the preference is set but no sound with the specified name is found in any of these places, the system's 'Glass' sound is used as the tunnel up sound, and the system's 'Basso' sound is used as the tunnel down sound.
    
     * Implements a new facility for installing configurations at the time Tunnelblick is installed, and updating them at any time thereafter.
      * Installation of these configurations is triggered by including them in 'Tunnelblick Configurations.bundle' in Tunnelblick.app/Contents/Resources/. Thus they can be distributed as part of the normal Tunnelblick update mechanism.
      * The configurations are 'Tunnelblick VPN Configurations' (.tblk packages) and include all the options such configurations provide.
      * The configurations may be updated automatically, separate from the application.
      * Configuration updates (like Tunnelblick updates) must be digitally signed, or be transmitted over an SSL connection.
      * Configuration updates do not modify Tunnelblick.app, so they do not require modifying the digital signature of Tunnelblick.app.
      * The 'Tunnelblick Configurations.bundle' includes Info.plist entries that specify a URL for checking for and obtaining updates.
    
     * Implements model code for a VPN service provider to allow signup for service from a Tunnelblick menu command. This includes several screens stepping the user through acceptance of terms of service, password assignment, etc. an interacts with a service provider's webserver. It is implemented as a compile-time option, turned off in the source code at present. (This was developed for a VPN service provider, but is being released under the GPL as a part of Tunnelblick.)
    
     * Implements a new 'universal login' facility that allows a single username/password combination stored in the Keychain to be used for all configurations that do not have a separate username/password combination in the Keychain. This facility was designed to be used by the above VPN service provider signup code (although it does not presently use it).

---
+++ tunnelblick/VPNServiceWelcomeController.m
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2011 Jonathan Bullard
+ *
+ *  This file is part of Tunnelblick.
+ *
+ *  Tunnelblick is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  Tunnelblick is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program (see the file COPYING included with this
+ *  distribution); if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  or see http://www.gnu.org/licenses/.
+ */
+
+
+#import "defines.h"
+
+#ifdef INCLUDE_VPNSERVICE
+
+#import "VPNServiceDefines.h"
+#import "VPNService.h"
+#import "VPNServiceWelcomeController.h"
+
+@interface VPNServiceWelcomeController() // Private methods
+
+-(NSTextFieldCell *) welcomeTFC;
+
+-(void)              setTitle: (NSString *) newTitle ofControl: (id) theControl;
+
+-(id)                delegate;
+
+@end
+
+@implementation VPNServiceWelcomeController
+
+-(id) initWithDelegate: (id) theDelegate
+{
+    if (  ![super initWithWindowNibName:@"VPNServiceWelcome"]  ) {
+        return nil;
+    }
+    
+    delegate = [theDelegate retain];
+    
+    return self;
+}
+
+-(void) awakeFromNib
+{
+    [[self window] setTitle: NSLocalizedString(@"Welcome!", @"Window title")];
+    
+    [self setTitle: NSLocalizedString(@"Next" , @"Button") ofControl: nextButton ];
+    
+    [[self welcomeTFC] setStringValue: NSLocalizedString(@"Congratulations! Your account has been created"
+                                                         " You should receive an email from Tunnelblick soon."
+                                                         " Follow the instructions in the email to verify your"
+                                                         " email address and then click 'Next'.", @"Window text")];
+    [[self window] center];
+    [NSApp activateIgnoringOtherApps:YES];
+    [[self window] makeKeyAndOrderFront: self];
+}
+
+// Sets the title for a control, shifting the origin of the control itself to the left, and the origin of other controls to the left or right to accomodate any change in width.
+-(void) setTitle: (NSString *) newTitle ofControl: (id) theControl
+{
+    NSRect oldRect = [theControl frame];
+    [theControl setTitle: newTitle];
+    [theControl sizeToFit];
+    
+    NSRect newRect = [theControl frame];
+    float widthChange = newRect.size.width - oldRect.size.width;
+    NSRect oldPos;
+    
+    if (   [theControl isEqual: nextButton]  ) {  // Shift the control itself left/right if necessary
+        oldPos = [theControl frame];
+        oldPos.origin.x = oldPos.origin.x - widthChange;
+        [theControl setFrame:oldPos];
+    }
+}
+
+- (void) dealloc
+{
+    [nextButton release];
+    
+    [welcomeTFC release];
+    
+    [delegate   release];
+    
+	[super dealloc];
+}
+
+- (NSString *) description
+{
+    return [NSString stringWithFormat:@"%@", [self class]];
+}
+
+- (IBAction) nextButtonWasClicked: sender
+{
+	[[self delegate] vpnServiceWelcome: self finishedWithChoice: VPNServiceWelcomeNextChoice];
+}
+
+-(NSTextFieldCell *) welcomeTFC
+{
+    return [[welcomeTFC retain] autorelease];
+}
+
+-(id) delegate
+{
+    return [[delegate retain] autorelease];
+}
+
+@end
+
+#endif
