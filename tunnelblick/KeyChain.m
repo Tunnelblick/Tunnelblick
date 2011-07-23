6b07d217e753e47776468ebf47c798a7b5aa6ccd
jkbullard <jkbullard@gmail.com>
2011-07-23 18:39:21 +0000

    Build 2695: Even more logging of status when password saves fail, and adds ability to create a release that is not digitally signed.

--- tunnelblick/KeyChain.m
+++ tunnelblick/KeyChain.m
@@ -63,40 +63,62 @@
     const char* service   = [serviceName UTF8String];
     const char* account   = [accountName UTF8String];
     
-    OSStatus status1 = SecKeychainFindGenericPassword(NULL,              // default keychain
-                                                      strlen(service),   // length of service name
-                                                      service,           // service name
-                                                      strlen(account),   // length of account name
-                                                      account,           // account name
-                                                      &passLength,       // length of password
-                                                      (void**)&passData, // address of password data as void **
-                                                      NULL               // we need no item reference
-                                                      );
-    if(passLength && status1 == noErr) {
-        NSString *returnPassword = [[[NSString alloc] initWithBytes:passData length:passLength encoding:NSUTF8StringEncoding] autorelease];
-        SecKeychainItemFreeContent(NULL,passData);	
-        return returnPassword;
+    OSStatus status = SecKeychainFindGenericPassword(NULL,              // default keychain
+                                                     strlen(service),   // length of service name
+                                                     service,           // service name
+                                                     strlen(account),   // length of account name
+                                                     account,           // account name
+                                                     &passLength,       // length of password
+                                                     (void**)&passData, // address of password data as void **
+                                                     NULL               // we need no item reference
+                                                     );
+    if (  status == noErr  ) {
+        if (  passLength != 0) {
+            NSString *returnPassword = [[[NSString alloc] initWithBytes:passData length:passLength encoding:NSUTF8StringEncoding] autorelease];
+            SecKeychainItemFreeContent(NULL,passData);
+            NSLog(@"Keychain item retrieved successfully for service = '%@' account = '%@'", serviceName, accountName);
+            return returnPassword;            
+        } else {
+            NSLog(@"Zero-length Keychain item retrieved for service = '%@' account = '%@'", serviceName, accountName);
+        }
+    } else {
+        if (  status == errKCItemNotFound  ) {
+            NSLog(@"Can't retrieve Keychain item for service = '%@' account = '%@' because it does not exist", serviceName, accountName);
+        } else {
+            NSLog(@"Can't retrieve Keychain item for service = '%@' account = '%@'; status was %d; error was %d:\n'%s'", serviceName, accountName, (int) status, (int) errno, strerror(errno));
+        }
     }
+    
     return nil;
 }
 
 - (int)setPassword:(NSString *)password
 {
+    if (  ! password  ) {
+        NSLog(@"Attempt to add nil Keychain item for service = '%@' account = '%@'", serviceName, accountName);
+        return -1;
+    }
+    
+    if (  [password length] == 0  ) {
+        NSLog(@"Attempt to add zero-length Keychain item for service = '%@' account = '%@'", serviceName, accountName);
+        return -1;
+    }
+    
     const char *cPassword = [password UTF8String];
     const char* service   = [serviceName UTF8String];
     const char* account   = [accountName UTF8String];
     
-    OSStatus status = SecKeychainAddGenericPassword(NULL,		// default keychain
-                                                    strlen(service),	// length of service name
-                                                    service,		// service name
-                                                    strlen(account),	// length of account name
-                                                    account,		// account name
-                                                    strlen(cPassword),	// length of password
-                                                    cPassword,		// pointer to password data
-                                                    NULL);		// we need no item reference
+    OSStatus status = SecKeychainAddGenericPassword(NULL,              // default keychain
+                                                    strlen(service),   // length of service name
+                                                    service,           // service name
+                                                    strlen(account),   // length of account name
+                                                    account,           // account name
+                                                    strlen(cPassword), // length of password
+                                                    cPassword,         // pointer to password data
+                                                    NULL);             // we need no item reference
     
     if (  status != noErr  ) {
-        NSLog(@"Can't save password; status was %d; error was %d:\n'%s'", (int) status, (int) errno, strerror(errno));
+        NSLog(@"Can't add Keychain item for service = '%@' account = '%@'; status was %d; error was %d:\n'%s'", serviceName, accountName, (int) status, (int) errno, strerror(errno));
     }
     
     return(status);
@@ -107,15 +129,26 @@
     const char* service   = [serviceName UTF8String];
     const char* account   = [accountName UTF8String];
     
-    OSStatus status1 = SecKeychainFindGenericPassword(NULL,              // default keychain
-                                                      strlen(service),   // length of service name
-                                                      service,           // service name
-                                                      strlen(account),   // length of account name
-                                                      account,           // account name
-                                                      NULL,       // length of password
-                                                      NULL, // address of password data as void **
+    OSStatus status = SecKeychainFindGenericPassword(NULL,             // default keychain
+                                                      strlen(service), // length of service name
+                                                      service,         // service name
+                                                      strlen(account), // length of account name
+                                                      account,         // account name
+                                                      NULL,            // length of password
+                                                      NULL,            // address of password data as void **
                                                       &itemRef               
                                                       );
-	if(status1 == noErr) SecKeychainItemDelete(itemRef);
+	if (  status == noErr  ) {
+        status = SecKeychainItemDelete(itemRef);
+        if (  status != noErr  ) {
+            NSLog(@"Can't delete Keychain item for service = '%@' account = '%@' after finding it; status was %d; error was %d:\n'%s'", serviceName, accountName, (int) status, (int) errno, strerror(errno));
+        }
+    } else {
+        if (  status == errKCItemNotFound  ) {
+            NSLog(@"Can't find Keychain item to delete for service = '%@' account = '%@' because it does not exist", serviceName, accountName);
+        } else {
+            NSLog(@"Can't find Keychain item to delete for service = '%@' account = '%@'; status was %d; error was %d:\n'%s'", serviceName, accountName, (int) status, (int) errno, strerror(errno));
+        }
+    }
 }
 @end
