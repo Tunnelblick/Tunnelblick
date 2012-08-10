8acbd41128d0e3e31c75cfe8891cfe0536820443
jkbullard@gmail.com <jkbullard@gmail.com@ecaf4b9d-c145-0410-a236-1b700048ef5c>
2012-08-10 18:47:42 +0000

    3.2 BRANCH: Build 2891.3097: Changes to Sparkle Updater to prepare for next release. Backport of r2094

---
+++ branches/3.2/third_party/Sparkle-1.5b6/Extras/Source Code/SUPlainInstaller.h
@@ -0,0 +1,20 @@
+//
+//  SUPlainInstaller.h
+//  Sparkle
+//
+//  Created by Andy Matuschak on 4/10/08.
+//  Copyright 2008 Andy Matuschak. All rights reserved.
+//
+
+#ifndef SUPLAININSTALLER_H
+#define SUPLAININSTALLER_H
+
+#import "Sparkle.h"
+#import "SUInstaller.h"
+#import "SUVersionComparisonProtocol.h"
+
+@interface SUPlainInstaller : SUInstaller { }
++ (void)performInstallationWithPath:(NSString *)path host:(SUHost *)host delegate:delegate synchronously:(BOOL)synchronously versionComparator:(id <SUVersionComparison>)comparator;
+@end
+
+#endif
