2f8dc5f8eb777ad7adbff5377523b5c97af7942d
jkbullard@gmail.com <jkbullard@gmail.com@ecaf4b9d-c145-0410-a236-1b700048ef5c>
2013-09-13 19:31:17 +0000

    Build 2580: Attempts to recover when OpenVPN crashes without having run the 'down' script by running the 'down' script from Tunnelblick.

--- trunk/tunnelblick/client.up.tunnelblick.sh
+++ trunk/tunnelblick/client.up.tunnelblick.sh
@@ -1117,6 +1117,7 @@ if ${ARG_TAP} ; then
 		logMessage "DEBUG_TAP: bRouteGatewayIsDhcp is TRUE"
 		if [ -z "$dev" ]; then
 			logMessage "Cannot configure TAP interface for DHCP without \$dev being defined. Exiting."
+            # We don't create the "/tmp/tunnelblick-downscript-needs-to-be-run.txt" file, because the down script does NOT need to be run since we didn't do anything
 			exit 1
 		fi
 		
@@ -1148,5 +1149,7 @@ else
 		EXIT_CODE=$?
 	fi
 fi
+
+touch "/tmp/tunnelblick-downscript-needs-to-be-run.txt"
 
 exit $EXIT_CODE
