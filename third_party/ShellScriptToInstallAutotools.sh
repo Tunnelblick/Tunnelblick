f6f61cebedce2a99dcb795e3010d734a2ed19db9
jkbullard <jkbullard@gmail.com>
2015-02-11 16:08:24 +0000

    Build 4220: Minor edits of third_party/README.txt and added shell script to install autotools

--- third_party/ShellScriptToInstallAutotools.sh
+++ third_party/ShellScriptToInstallAutotools.sh
@@ -4,33 +4,53 @@
 # This script downloads and installs new versions of the GNU "autotools" -- a set of
 # command line tools that are obsolete in Xcode 3 and are missing from Xcode 4 and higher.
 #
-# It downloads the following version of each tool (change to download different versions):
-
-readonly autoconf_version=autoconf-2.69
-readonly automake_version=automake-1.9
-readonly libtool_version=libtool-2.4.5
-
 # It installs the tools to /usr/local/bin. It uses sudo to install because /usr/local/bin
 # is a protected folder. So it must be run from an OS X account with administrator
-# privileges, and will ask for the the user's password.
+# privileges. (Sudo will ask for the the user's password once.)
 
-# Download and build the tools in the user's "Downloads" folder.
-readonly downloads_folder_path=~/Downloads
 
+
+# Version of each tool to download and install:
+
+readonly autoconf_version="autoconf-2.69"
+readonly automake_version="automake-1.9"
+readonly libtool_version="libtool-2.4.5"
+
+# You can check for the latest version of each tool by examining the following:
+#     http://ftpmirror.gnu.org/autoconf
+#     http://ftpmirror.gnu.org/automake
+#     http://ftpmirror.gnu.org/libtool
+# If you wish to install different versions, modify the variables above.
+# Note that it is possible (although unlikely) that later versions of the autotools may
+# not work properly for the third party build process.
+
+
+
+# Folder to which the tools are downloaded and in which the tools are built:
+
+readonly downloads_folder_path=~/"Downloads"
+
+# Note that if you modify this path, you should be sure to either have no spaces in the
+# path or use double-quotes to enclose the path. If you use double-quotes with ~/, be sure
+# the first double-quote follows the '~/'.
+
+
+
+# URLs for downloading the tools:
 readonly autoconf_url=http://ftpmirror.gnu.org/autoconf/${autoconf_version}.tar.gz
 readonly automake_url=http://ftpmirror.gnu.org/automake/${automake_version}.tar.gz
 readonly libtool_url=http://ftpmirror.gnu.org/libtool/${libtool_version}.tar.gz
 
-if [ ! -d ${downloads_folder_path} ] ; then
-    echo "Downloads folder '${downloads_folder_path}' does not exist"
+if [ ! -d "${downloads_folder_path}" ] ; then
+    echo "Downloads folder '${downloads_folder_path}' does not exist or is not a folder"
     exit 1
 fi
 
 echo "INSTALLING AUTOCONF:"
 cd "${downloads_folder_path}"
-curl -OL ${autoconf_url}
-tar xzf ${autoconf_version}.tar.gz
-cd ${autoconf_version}
+curl -OL "${autoconf_url}"
+tar xzf "${autoconf_version}.tar.gz"
+cd "${autoconf_version}"
 ./configure
 make
 sudo make install
@@ -42,18 +62,18 @@ export "PATH=/usr/local/bin:$PATH"
 
 echo "INSTALLING AUTOMAKE:"
 cd "${downloads_folder_path}"
-curl -OL ${automake_url}
-tar xzf ${automake_version}.tar.gz
-cd ${automake_version}
+curl -OL "${automake_url}"
+tar xzf "${automake_version}.tar.gz"
+cd "${automake_version}"
 ./configure
 make
 sudo make install
 
 echo "INSTALLING LIBTOOL:"
 cd "${downloads_folder_path}"
-curl -OL ${libtool_url}
-tar xzf ${libtool_version}.tar.gz
-cd ${libtool_version}
+curl -OL "${libtool_url}"
+tar xzf "${libtool_version}.tar.gz"
+cd "${libtool_version}"
 ./configure
 make
 sudo make install
