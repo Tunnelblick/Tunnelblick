9b29144c5ce9e0f806bea9742cbe2bb17af8968f
jkbullard@gmail.com <jkbullard@gmail.com@ecaf4b9d-c145-0410-a236-1b700048ef5c>
2015-05-25 17:51:44 +0000

    Build 4318: Launch Tunnelblick at login if any 'openvpn' processes are running (or 'openvpnstart' or 'tunnelblick-helper') or if it was running when the user last logged in

--- trunk/tunnelblick/launchAtLogin.sh
+++ trunk/tunnelblick/launchAtLogin.sh
@@ -3,7 +3,29 @@ trap "" TSTP
 trap "" HUP
 trap "" INT
 export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
-launch="$( defaults read net.tunnelblick.tunnelblick launchAtNextLogin 2> /dev/null )"
-if [ "${launch}" = "1" ] ; then
+
+# Launch Tunnelblick if the 'launchAtNextLogin' preference is "1" or OpenVPN is running.
+#
+# The preference is set to "1" when you launch Tunnelblick and is set to "0" when you
+# quit Tunnelblick and you are not logging out.
+# So Tunnelblick is launched only if it was running when you logged out or shut down
+# or restarted the computer or a process whose name contains 'openvpn' or 'tunnelblick-helper'
+# is running. (This includes 'openvpnstart' and 'tunnelblick-helper' to handle the case where
+# OpenVPN is about to be started but has not yet been started.)
+
+launch_at_login_preference="$( defaults read net.tunnelblick.tunnelblick launchAtNextLogin 2> /dev/null )"
+if [ "${launch_at_login_preference}" = "1" ] ; then
   open /Applications/Tunnelblick.app
+fi
+
+processes="$(ps -ef)"
+
+openvpn_is_running="$( echo "${processes}" | grep -w openvpn | grep -v grep )"
+if [ "${openvpn_is_running}" != "" ] ; then
+  open /Applications/Tunnelblick.app
+fi
+
+helper_is_running="$( echo "${processes}" | grep -w tunnelblick-helper | grep -v grep )"
+if [ "${helper_is_running}" != "" ] ; then
+open /Applications/Tunnelblick.app
 fi
