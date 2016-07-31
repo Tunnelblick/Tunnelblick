eb5243e298184f407397fb806818bb33d7692b77
Ludovic LANGE <github@lange.nom.fr>
2016-08-01 01:35:43 +0200

    Handle OpenVPN Challenge/Response static protocol.
    Static protocol is activated when the configuration file (of the
    client) contains the 'static-challenge' directive.
    In that case, OpenVPN's management interface asks for the
    challenge (the text of the challenge comes from the
    configuration file - 'static-challenge' directive).
    
    We build a popup window asking for the response
    to that challenge, and we present the response to the server
    (via the management interface).
    
    [Documentation of static protocol](https://github.com/OpenVPN/openvpn/blob/master/doc/management-notes.txt#L1022)

--- tunnelblick/VPNConnection.m
+++ tunnelblick/VPNConnection.m
@@ -48,6 +48,7 @@
 #import "TBUserDefaults.h"
 #import "UIHelper.h"
 #import "VPNConnection.h"
+#import "MF_Base64Additions.h"
 
 extern NSMutableArray       * gConfigDirs;
 extern NSString             * gPrivatePath;
@@ -3150,6 +3151,30 @@ static pthread_mutex_t lastStateMutex = PTHREAD_MUTEX_INITIALIZER;
 	}
 }
 
+- (NSString *)getResponseFromChallenge: (NSString *)challenge echoResponse: (BOOL)echoResponse{
+    NSAlert *alert = [NSAlert alertWithMessageText: challenge
+                                     defaultButton:@"OK"
+                                   alternateButton:@"Cancel"
+                                       otherButton:nil
+                         informativeTextWithFormat:@""];
+
+    NSTextField *input;
+    input = [[NSSecureTextField alloc] initWithFrame:NSMakeRect(0, 0, 200, 24)];
+    [input autorelease];
+    [alert setAccessoryView:input];
+    [[NSRunningApplication currentApplication] activateWithOptions:NSApplicationActivateIgnoringOtherApps];
+    [[alert window] setInitialFirstResponder: input];
+    NSInteger button = [alert runModal];
+    if (button == NSAlertDefaultReturn) {
+        [input validateEditing];
+        return [input stringValue];
+    } else if (button == NSAlertAlternateReturn) {
+        return nil;
+    } else {
+        NSAssert1(NO, @"Invalid input dialog button %ld", (long)button);
+        return nil;
+    }
+}
 
 - (void) processLine: (NSString*) line
 {
@@ -3434,7 +3459,30 @@ static pthread_mutex_t lastStateMutex = PTHREAD_MUTEX_INITIALIZER;
     authFailed = FALSE;
     credentialsAskedFor = FALSE;
     userWantsState = userWantsUndecided;
-    
+
+    BOOL echoResponse = FALSE;
+    NSString * challengeText = nil;
+
+    if (   [line rangeOfString: @" SC:0,"].length
+        || [line rangeOfString: @" SC:1,"].length  ) {
+
+        NSRange rngStartChallenge = [line rangeOfString: @" SC:"];
+        NSAssert(rngStartChallenge.length != 0, @"rngStartChallenge.length should not be 0, we tested it before...");
+
+        TBLog(@"DB-AU", @"processLine: Server asking for Static Challenge");
+        if (  rngStartChallenge.length != 0  ) {
+            NSString * afterStartChallenge = [line substringFromIndex: rngStartChallenge.location + 4];
+            NSRange rngComma = [afterStartChallenge rangeOfString: @","];
+
+            NSString * echoResponseStr = [afterStartChallenge substringToIndex: rngComma.location];
+            echoResponse = [echoResponseStr isEqualToString:@"1"];
+
+            TBLog(@"DB-AU", @"echoResponse is '%d'", echoResponse);
+            challengeText = [afterStartChallenge substringFromIndex: rngComma.location + 1];
+            TBLog(@"DB-AU", @"challengeText is '%@'", challengeText);
+        }
+    }
+
     // Find out whether the server wants a private key or user/auth:
     NSRange pwrange_need = [parameterString rangeOfString: @"Need \'"];
     NSRange pwrange_password = [parameterString rangeOfString: @"\' password"];
@@ -3488,8 +3536,17 @@ static pthread_mutex_t lastStateMutex = PTHREAD_MUTEX_INITIALIZER;
                 [self addToLog: [NSString stringWithFormat: @"*Tunnelblick: Disconnecting; username is %ld bytes long; password is %ld bytes long; each is limited to %ld bytes", (long)strlen(usernameC), (long)strlen(passwordC), (long)MAX_LENGTH_OF_QUOTED_MANGEMENT_INTERFACE_PARAMETER]];
                 [self startDisconnectingUserKnows: [NSNumber numberWithBool: NO]];
             } else {
+                NSString * response = nil;
+                if (challengeText) {
+                    response = [self getResponseFromChallenge: challengeText echoResponse: echoResponse];
+                    TBLog(@"DB-AU", @"response is '%@'", response);
+                }
                 [self sendStringToManagementSocket:[NSString stringWithFormat:@"username \"Auth\" \"%@\"\r\n", escaped(myUsername)] encoding:NSUTF8StringEncoding];
-                [self sendStringToManagementSocket:[NSString stringWithFormat:@"password \"Auth\" \"%@\"\r\n", escaped(myPassword)] encoding:NSUTF8StringEncoding];
+                if (!response) {
+                    [self sendStringToManagementSocket:[NSString stringWithFormat:@"password \"Auth\" \"%@\"\r\n", escaped(myPassword)] encoding:NSUTF8StringEncoding];
+                } else {
+                    [self sendStringToManagementSocket:[NSString stringWithFormat:@"password \"Auth\" \"SCRV1:%@:%@\"\r\n", [myPassword base64String], [response base64String]] encoding:NSUTF8StringEncoding];
+                }
             }
         } else {
             [self addToLog: @"*Tunnelblick: Disconnecting; user cancelled authorization"];
