7b363f9a9edc68f031f703751becd7a76289e506
Jonathan Bullard <jkbullard@gmail.com>
2016-09-11 07:39:25 -0400

    Clarify that two files are bash scripts

--- tunnelblick/BuildThirdPartyItems.sh
+++ tunnelblick/BuildThirdPartyItems.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 by Jonathan K. Bullard. All rights reserved.
+#
+# This file is part of Tunnelblick.
+#
+# Tunnelblick is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2
+# as published by the Free Software Foundation.
+#
+# Tunnelblick is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (see the file COPYING included with this
+# distribution); if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# or see http://www.gnu.org/licenses/.
+#
+#
 # This build phase builds the third party items used by Tunnelblick.
 #
 # Because these items rarely change but take a lot of time to build, they are not
