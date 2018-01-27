7ed6faf71f03956c0c4e81f382ddc32272312628
Tunnelblick developer <jkbullard@gmail.com>
2018-01-27 12:11:08 -0500

    Launch Tunnelblick at login if network access has been disabled

--- tunnelblick/launchAtLogin.sh
+++ tunnelblick/launchAtLogin.sh
@@ -4,10 +4,18 @@ trap "" HUP
 trap "" INT
 export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
 
-# Launch Tunnelblick if the 'launchAtNextLogin' preference is "1" or OpenVPN is running or is about to be run.
+# Launch Tunnelblick if:
+#    A. Tunnelblick has disabled network access; or
+#    B. The 'launchAtNextLogin' preference is "1"; or
+#    C. OpenVPN is running or is about to be run.
 #
-# The preference is set to "1" when you launch Tunnelblick if the 'doNotLaunchOnLogin' preference is not "1".
-# The preference is set to "0" when you quit Tunnelblick and you are not logging out.
+# The 'launchAtNextLogin' preference is set to "1" when you launch Tunnelblick if the 'doNotLaunchOnLogin' preference is not "1".
+# The 'launchAtNextLogin' preference is set to "0" when you quit Tunnelblick and you are not logging out.
+
+if [ -e "/Library/Application Support/Tunnelblick/disabled-network-services.txt" ] ; then
+  open /Applications/Tunnelblick.app
+  exit
+fi
 
 launch_at_login_preference="$( defaults read net.tunnelblick.tunnelblick launchAtNextLogin 2> /dev/null )"
 if [ "${launch_at_login_preference}" = "1" ] ; then
