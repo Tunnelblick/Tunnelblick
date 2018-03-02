Tunnelblick/Tunnelblick

commit 676ea258039ba2b0eead9219c808184820efd3da

Fix bad sorting of OpenVPN versions on High Sierra that caused hang

tunnelblick/MenuController.m
@@ -4398,6 +4398,8 @@ static void signal_handler(int signalNumber)
         }
     }
     
+	[nameArray sortUsingSelector:@selector(compare:)];
+	
     if (  [nameArray count] == 0  ) {
         NSLog(@"There are no versions of OpenVPN in this copy of Tunnelblick");
         [self terminateBecause: terminatingBecauseOfError];

commit c041ce25917d1919b6024f45cbebde8e62c74c86

Fix omitting OpenVPN version if no LibreSSL-linked copy
Caused by off-by-one error introduced in 676ea25

tunnelblick/MenuController.m
@@ -4409,7 +4409,7 @@ static void signal_handler(int signalNumber)
 	// Sort the array so OpenSSL comes before LibreSSL
 	NSMutableArray * sortedNames = [[[NSMutableArray alloc] initWithCapacity: [nameArray count]] autorelease];
 	NSUInteger ix;
-	for (  ix=0; ix<[nameArray count]-1; ix++  ) {
+	for (  ix=0; ix<[nameArray count]; ix++  ) {
 		if (   (ix != [nameArray count]-1)
             && ([[nameArray objectAtIndex: ix]     rangeOfString: @"libressl"].length != 0)
 			&& ([[nameArray objectAtIndex: ix + 1] rangeOfString: @"openssl"].length  != 0)  ) {