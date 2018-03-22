6579bf6a85974559dc563143937b5807d86516f8
Tunnelblick developer <jkbullard@gmail.com>
2018-03-22 17:23:19 -0400

    Fix string that wasn't localized

--- tunnelblick/SystemAuth.m
+++ tunnelblick/SystemAuth.m
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Jonathan K. Bullard
+ * Copyright 2016, 2018 Jonathan K. Bullard
  *
  *  This file is part of Tunnelblick.
  *
@@ -225,7 +225,7 @@ TBSYNTHESIZE_OBJECT(retain, NSString *, prompt, setPrompt)
     allowReactivation = reactivateOk;
     
     if (  [SystemAuth haveValidLockSystemAuth ] ) {
-        NSString * expandedPrompt = [NSString stringWithFormat: @"%@\n\n Note: Tunnelblick is in administrator mode, so a computer administrator username and password are not required.", prompt];
+        NSString * expandedPrompt = [NSString stringWithFormat: NSLocalizedString(@"%@\n\n Note: Tunnelblick is in administrator mode, so a computer administrator's authorization is not required.", @"Window text. The '%@' at the beginning is a separately translated string that describes an action that is to be taken."), prompt];
         int result = TBRunAlertPanelExtended(NSLocalizedString(@"Tunnelblick", @"Window title"),
                                              expandedPrompt,
                                              NSLocalizedString(@"OK", @"Button"),     // Default button
