648cdb8fc3ca5cebb7ddaa669cdded9c6017e9b3
Jonathan K. Bullard <jkbullard@gmail.com>
2018-09-11 08:39:30 -0400

    Remove code merged in error (openSetup)

--- tunnelblick/MenuController.m
+++ tunnelblick/MenuController.m
@@ -3786,13 +3786,6 @@ static void signal_handler(int signalNumber)
     return [self application: nil openFiles: filePaths];
 }
 
--(BOOL) openSetup: (NSArray * ) filePaths {
-
-	// Invoked from MainIconView to implement files dropped on the icon for installation.
-	
-	return [ConfigurationManager importTunnelblickSetup: filePaths];
-}
-
 -(void) setupUpdaterAutomaticChecks {
     
     if (  [updater respondsToSelector: @selector(setAutomaticallyChecksForUpdates:)]  ) {
