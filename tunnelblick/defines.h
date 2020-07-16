4b73292db4e44571258f2c34c44e23a6866a4b44
jkbullard <jkbullard@gmail.com>
2020-07-16 16:23:20 -0400

    --ns-cert-type will not be removed in OpenVPN 2.5. Maybe 2.6 or 2.7.

--- tunnelblick/defines.h
+++ tunnelblick/defines.h
@@ -223,9 +223,10 @@
                             @[@"2.3", @"2.4",  @"tls-remote"], \
                             @[@"2.3", @"2.5",  @"compat-names", @"no-name-remapping"], \
                             @[@"2.4", @"2.4",  @"tun-ipv6"], \
-                            @[@"2.4", @"2.5",  @"client-cert-not-required", @"key-method", @"no-iv", @"no-replay", @"ns-cert-type"], \
+                            @[@"2.4", @"2.5",  @"client-cert-not-required", @"key-method", @"no-iv", @"no-replay"], \
                             @[@"2.4", @"2.5?", @"comp-lzo", @"dhcp-release", @"max-routes", @"secret"], \
                             @[@"2.4", @"2.6",  @"keysize"], \
+                            @[@"2.4", @"2.6?", @"ns-cert-type"], \
                             @[@"2.5", @"2.6?", @"compress", @"ncp-disable"] \
                             ];
 
