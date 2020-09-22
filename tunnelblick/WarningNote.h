e85295b9e18350160dbd46b44245e20a6360fa1e
jkbullard <jkbullard@gmail.com>
2020-09-22 08:58:59 -0400

    Implement WarningNote class

---
+++ tunnelblick/WarningNote.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Jonathan K. Bullard. All rights reserved.
+ *
+ *  This file is part of Tunnelblick.
+ *
+ *  Tunnelblick is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  Tunnelblick is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program (see the file COPYING included with this
+ *  distribution); if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  or see http://www.gnu.org/licenses/.
+ */
+
+#import "defines.h"
+
+@interface WarningNote : NSObject {
+
+    NSString           * headline;
+    NSAttributedString * message;
+    NSString           * preferenceKey;
+    NSString           * index;
+}
+
+-(WarningNote *) initWithHeadline: (NSString *)           theHeadline
+                          message: (NSAttributedString *) theMessage
+                    preferenceKey: (NSString *)           thePreferenceKey
+                            index: (NSString *)           theIndex;
+
+-(NSString *) headline;
+
+-(NSString *) preferenceKey;
+
+-(void) showWarning: (id) sender;
+
+@end
+
---
+++ tunnelblick/WarningNote.m
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020 Jonathan K. Bullard. All rights reserved.
+ *
+ *  This file is part of Tunnelblick.
+ *
+ *  Tunnelblick is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  Tunnelblick is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program (see the file COPYING included with this
+ *  distribution); if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  or see http://www.gnu.org/licenses/.
+ */
+
+#import "WarningNote.h"
+
+#import "helper.h"
+
+#import "MenuController.h"
+
+extern MenuController * gMC;
+
+@implementation WarningNote
+
+-(WarningNote *) initWithHeadline: (NSString *)           theHeadline
+                          message: (NSAttributedString *) theMessage
+                    preferenceKey: (NSString *)           thePreferenceKey
+                            index: (NSString *)           theIndex {
+
+    self = [super init];
+    if (  self  ) {
+        headline      = [theHeadline retain];
+        message       = [theMessage retain];
+        preferenceKey = [thePreferenceKey retain];
+        index         = [theIndex retain];
+    }
+
+    return self;
+}
+
+-(NSString *) headline {
+
+    return [[headline retain] autorelease];
+}
+
+-(NSString *) preferenceKey {
+
+    return [[preferenceKey retain] autorelease];
+}
+
+
+-(void) showWarning: (id) sender {
+
+    (void) sender;
+    
+    TBShowAlertWindowExtended(NSLocalizedString(@"Tunnelblick", @"Window title"), message, preferenceKey, preferenceKey, @1, nil, nil, NO);
+
+    TBShowAlertWindowRemoveFromCache(preferenceKey, [message string]);
+}
+
+-(void) dealloc {
+    
+    [headline      release];
+    [message       release];
+    [preferenceKey release];
+    [index         release];
+    
+    [super dealloc];
+}
+
+@end
