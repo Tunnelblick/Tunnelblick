Tunnelblick/Tunnelblick

commit 5a21c3832089dcaca958dcb32f4ee0d702aad78e

Enhance security of the management interface password
 * Double the length to 256 bits
 * Create a new password for each connection to a VPN

tunnelblick/tunnelblick-helper.m
@@ -28,6 +28,7 @@
 #import <sys/acl.h>
 #import <sys/mount.h>
 #import <sys/param.h>
+#import <sys/stat.h>
 #import <sys/sysctl.h>
 #import <sys/types.h>
 #import <sys/xattr.h>
@@ -910,7 +911,20 @@ NSString * newTemporaryDirectoryPathInTunnelblickHelper(void) {
 
 //**************************************************************************************************************************
 
-int runAsRootWithConfigNameAndLocCodeReturnOutput(NSString * thePath, NSArray * theArguments, mode_t permissions, NSString * configName, unsigned configLocCode, NSString ** stdOut, NSString ** stdErr) {
+NSString * managementPasswordFilePath(NSString * configName) {
+    
+    if (  configName  ) {
+        NSString * name = [configName stringByAppendingPathExtension: @"mip"];
+        NSString * path = [L_AS_T_MIPS stringByAppendingPathComponent: name];
+        return path;
+    }
+    
+    return nil;
+}
+
+//**************************************************************************************************************************
+
+int runAsRootWithConfigNameAndLocCodeAndmanagementPasswordReturnOutput(NSString * thePath, NSArray * theArguments, mode_t permissions, NSString * configName, unsigned configLocCode, NSString * managementPassword, NSString ** stdOut, NSString ** stdErr) {
 
 	// Runs a program as root
 	//
@@ -977,14 +991,66 @@ int runAsRootWithConfigNameAndLocCodeReturnOutput(NSString * thePath, NSArray * 
     }
     [task setStandardError: errFileHandle];
     
+    if (  managementPassword  ) {
+        
+        // Create a file with the contents of the management interface password followed by a linefeed
+        // The file must be owned by root and have 0700 permissions so only root can read it.
+        
+        NSString * path = managementPasswordFilePath(configName);
+        
+        const char * path_c = [path fileSystemRepresentation];
+
+        mode_t old_umask = umask(0077);
+        
+        becomeRoot(@"Create .mip file");
+        
+            FILE * file = fopen(path_c, "w");
+            if (  file == NULL  ) {
+                umask(old_umask);
+                stopBeingRoot();
+                fprintf(stderr, "runAsRoot: Unable to create %s\n", path_c);
+                [dirPath release];
+                [stdFileHandle release];
+                [errFileHandle release];
+                return -1;
+            }
+
+            umask(old_umask);
+
+            const char * managementPassword_c = [[managementPassword stringByAppendingString: @"\n"]
+                                            cStringUsingEncoding: NSASCIIStringEncoding];
+            size_t len = strlen(managementPassword_c);
+            size_t written = fwrite(managementPassword_c, 1, len, file);
+            if (  written != len  ) {
+                stopBeingRoot();
+                fprintf(stderr, "runAsRoot: Unable to write to %s\n", path_c);
+                [dirPath release];
+                [stdFileHandle release];
+                [errFileHandle release];
+                return -1;
+            }
+            
+            if (  fclose(file) != 0  ) {
+                stopBeingRoot();
+                fprintf(stderr, "runAsRoot: Unable to close %s\n", path_c);
+                [dirPath release];
+                [stdFileHandle release];
+                [errFileHandle release];
+                return -1;
+            }
+        
+        stopBeingRoot();
+    }
+    
     [task setCurrentDirectoryPath: @"/private/tmp"];
     
     [task setEnvironment: getSafeEnvironment(configName, configLocCode, nil)];
     
-	becomeRoot([NSString stringWithFormat: @"launch %@", [thePath lastPathComponent]]);
+    becomeRoot([NSString stringWithFormat: @"launch %@", [thePath lastPathComponent]]);
     
-	[task launch];
-	[task waitUntilExit];
+        [task launch];
+        [task waitUntilExit];
+    
     stopBeingRoot();
 	
     [stdFileHandle closeFile];
@@ -1038,20 +1104,20 @@ int runAsRootWithConfigNameAndLocCodeReturnOutput(NSString * thePath, NSArray * 
 //**************************************************************************************************************************
 int runAsRootWithConfigNameAndLocCode(NSString * thePath, NSArray * theArguments, mode_t permissions, NSString * configName, unsigned configLocCode) {
     
-    return runAsRootWithConfigNameAndLocCodeReturnOutput(thePath, theArguments, permissions, configName, configLocCode, nil, nil);
+    return runAsRootWithConfigNameAndLocCodeAndmanagementPasswordReturnOutput(thePath, theArguments, permissions, configName, configLocCode, nil, nil, nil);
     
 }
 
 //**************************************************************************************************************************
 
 int runAsRoot(NSString * thePath, NSArray * theArguments, mode_t permissions) {
-	return runAsRootWithConfigNameAndLocCode(thePath, theArguments, permissions, nil, 0);
+	return runAsRootWithConfigNameAndLocCodeAndmanagementPasswordReturnOutput(thePath, theArguments, permissions, nil, 0, nil, nil, nil);
 }
 
 //**************************************************************************************************************************
 
 int runAsRootReturnOutput(NSString * thePath, NSArray * theArguments, mode_t permissions, NSString ** stdOut, NSString ** stdErr) {
-    return runAsRootWithConfigNameAndLocCodeReturnOutput(thePath, theArguments, permissions, nil, 0, stdOut, stdErr);
+    return runAsRootWithConfigNameAndLocCodeAndmanagementPasswordReturnOutput(thePath, theArguments, permissions, nil, 0, nil, stdOut, stdErr);
 }
 
 //**************************************************************************************************************************
@@ -2324,7 +2390,8 @@ int startVPN(NSString * configFile,
              BOOL       noMonitor,
              unsigned   bitMask,
              NSString * leasewatchOptions,
-             NSString * openvpnVersion) {
+             NSString * openvpnVersion,
+             NSString * managementPassword) {
     
 	// Tries to start an openvpn connection (up to ten times if not starting from GUI).
     // Returns OPENVPNSTART_COULD_NOT_START_OPENVPN (having output a message to stderr) if any other error occurs
@@ -2519,13 +2586,7 @@ int startVPN(NSString * configFile,
     [arguments addObject: @"--management"];
     [arguments addObject: @"127.0.0.1"];
     [arguments addObject: [NSString stringWithFormat:@"%u", port]];
-	
-	NSString * themipName = mipName();
-	if (  ! themipName  ) {
-		fprintf(stderr, "Unable to find .mip\n");
-		exitOpenvpnstart(169);
-	}
-	[arguments addObject: [L_AS_T stringByAppendingPathComponent: [themipName stringByAppendingString: @".mip"]]];
+    [arguments addObject: managementPasswordFilePath(configFile)];
     
 	if (  (bitMask & OPENVPNSTART_TEST_MTU) != 0  ) {
         [arguments addObject: @"--mtu-test"];
@@ -2859,7 +2920,7 @@ int startVPN(NSString * configFile,
             }
 		}
     }
-	
+    
     // Unload foo.tun/tap iff we are loading the new net.tunnelblick.tun/tap and foo.tun/tap are loaded
     unsigned unloadMask  = 0;
     unsigned loadedKexts = getLoadedKextsMask();
@@ -2957,19 +3018,19 @@ int startVPN(NSString * configFile,
 		}
 	}
     
-    status = runAsRoot(openvpnPath, arguments, 0755);
+    status = runAsRootWithConfigNameAndLocCodeAndmanagementPasswordReturnOutput(openvpnPath, arguments, 0755, configFile, 0, managementPassword, nil, nil);
     
     NSMutableString * displayCmdLine = [NSMutableString stringWithFormat: @"     %@", openvpnPath];
     unsigned i;
     for (i=0; i<[arguments count]; i++) {
-		NSString * arg = [arguments objectAtIndex: i];
-		if (  [arg hasPrefix: @"--"]  ) {
-			[displayCmdLine appendString: [NSString stringWithFormat: @"\n     %@", arg]];
-		} else {
-			[displayCmdLine appendString: [NSString stringWithFormat: @" %@", arg]];
-		}
-	}
-    
+        NSString * arg = [arguments objectAtIndex: i];
+        if (  [arg hasPrefix: @"--"]  ) {
+            [displayCmdLine appendString: [NSString stringWithFormat: @"\n     %@", arg]];
+        } else {
+            [displayCmdLine appendString: [NSString stringWithFormat: @" %@", arg]];
+        }
+    }
+
     if (  status != 0  ) {
         NSString * logContents = @"";
         if (  (bitMask & OPENVPNSTART_DISABLE_LOGGING) == 0  ) {
@@ -3555,7 +3616,8 @@ int main(int argc, char * argv[]) {
             unsigned   bitMask = OPENVPNSTART_KEXTS_MASK_LOAD_DEFAULT;
             NSString * leasewatchOptions = @"-i";
             NSString * openvpnVersion = @"";
-            
+            NSString * managementPassword = @"";
+
 			if (  (argc > 3) && (argc <= OPENVPNSTART_MAX_ARGC)  ) {
                 
                 if (  strlen(argv[3]) <= DISPLAY_NAME_LENGTH_MAX                      ) configFile = [NSString stringWithUTF8String:argv[2]];
@@ -3567,7 +3629,8 @@ int main(int argc, char * argv[]) {
                 if (  (argc >  8) && (strlen(argv[ 8]) < 10)                          ) bitMask = cvt_atou(argv[8], @"bitMask");
                 if (  (argc >  9) && (strlen(argv[ 9]) < 16)                          ) leasewatchOptions = [NSString stringWithUTF8String: argv[9]]; 
                 if (  (argc > 10) && (strlen(argv[10]) < 128)                         ) openvpnVersion    = [NSString stringWithUTF8String: argv[10]];
-                
+                if (  (argc > 11) && (strlen(argv[11]) < 128)                         ) managementPassword = [NSString stringWithUTF8String: argv[11]];
+
                 validateConfigName(configFile);
                 validatePort(port);
                 validateUseScripts(useScripts);
@@ -3614,7 +3677,8 @@ int main(int argc, char * argv[]) {
                                        noMonitor,
                                        bitMask,
                                        leasewatchOptions,
-                                       openvpnVersion);
+                                       openvpnVersion,
+                                       managementPassword);
                     
                     if (   (retCode == 0)               // If succeeded, return indicating that success
                         || ((bitMask & OPENVPNSTART_NOT_WHEN_COMPUTER_STARTS) != 0)  ) {// If failed and are using the GUI, return the failure

tunnelblick/MenuController.m
@@ -7559,16 +7559,6 @@ BOOL warnAboutNonTblks(void)
     return -1;
 }
 
-BOOL needToCreateMip(void) {
-	
-	if (  mipName()  ) {
-		return NO;
-	}
-	
-	NSLog(@"No .mip");
-	return YES;
-}
-
 // Checks whether the installer needs to be run
 //
 // Returns an unsigned containing INSTALLER_... bits set appropriately for runInstaller:, and, ultimately, by the installer program
@@ -7580,7 +7570,6 @@ unsigned needToRunInstaller(BOOL inApplications)
     unsigned flags = 0;
     
     if (  needToChangeOwnershipAndOrPermissions(inApplications)  ) flags = flags | INSTALLER_SECURE_APP;
-	if (  ( needToCreateMip() )									 ) flags = flags | INSTALLER_SECURE_APP; // .mip will be created by any use of installer
     if (  needToReplaceLaunchDaemon()                            ) flags = flags | INSTALLER_REPLACE_DAEMON;
     if (  needToRepairPackages()                                 ) flags = flags | INSTALLER_SECURE_TBLKS;
     if (  needToMoveLibraryOpenVPN()                             ) flags = flags | INSTALLER_MOVE_LIBRARY_OPENVPN;
@@ -7956,6 +7945,16 @@ BOOL needToChangeOwnershipAndOrPermissions(BOOL inApplications)
         return YES;
     }
     if (  ! checkOwnerAndPermissions(L_AS_T_LOGS, 0, 0, PERMS_SECURED_FOLDER)  ) {
+        return YES; // NSLog already called
+    }
+    
+    // check that Mips directory exists and has proper ownership and permissions
+    if (  ! (   [gFileMgr fileExistsAtPath: L_AS_T_MIPS isDirectory: &isDir]
+             && isDir )  ) {
+        NSLog(@"Need to create mips directory '%@'", L_AS_T_MIPS);
+        return YES;
+    }
+    if (  ! checkOwnerAndPermissions(L_AS_T_MIPS, 0, 0, PERMS_SECURED_FOLDER)  ) {
         return YES; // NSLog already called
     }
     

tunnelblick/VPNConnection.h
@@ -106,6 +106,8 @@ struct Statistics {
 	NSString	  * dynamicChallengeFlags;
 	NSString      * authRetryParameter;		// Parameter from auth-retry as seen in configuration file (or nil if not seen)
 	
+    NSString     * managementPassword;
+    
 	pid_t           pid;                // 0, or process ID of OpenVPN process created for this connection
 	unsigned int    portNumber;         // 0, or port number used to connect to management socket
     volatile int32_t avoidHasDisconnectedDeadlock; // See note at start of 'hasDisconnected' method
@@ -315,6 +317,7 @@ TBPROPERTY(          NSString *,               dynamicChallengeState,           
 TBPROPERTY(          NSString *,               dynamicChallengePrompt,           setDynamicChallengePrompt)
 TBPROPERTY(          NSString *,               dynamicChallengeFlags,            setDynamicChallengeFlags)
 TBPROPERTY(          NSString *,               authRetryParameter,               setAuthRetryParameter)
+TBPROPERTY(          NSString *,               managementPassword,               setManagementPassword)
 TBPROPERTY(          NSString *,               serverIPAddress,                  setServerIPAddress)
 TBPROPERTY(          NSString *,               connectedCfgLocCodeString,        setConnectedCfgLocCodeString)
 TBPROPERTY(          BOOL,                     ipCheckLastHostWasIPAddress,      setIpCheckLastHostWasIPAddress)

tunnelblick/VPNConnection.m
@@ -253,7 +253,7 @@ TBPROPERTY(          NSMutableArray *,         messagesIfConnectionFails,       
         }
         statistics.lastSet = [[NSDate date] retain];
         
-        [self clearStatisticsIncludeTotals: YES];        
+        [self clearStatisticsIncludeTotals: YES];
     }
     
     return self;
@@ -919,7 +919,8 @@ TBPROPERTY(          NSMutableArray *,         messagesIfConnectionFails,       
 	[dynamicChallengeFlags            release]; dynamicChallengeFlags            = nil;
 	[authRetryParameter				  release]; authRetryParameter               = nil;
 	[statistics.lastSet               release]; statistics.lastSet               = nil;
-	
+    [managementPassword               release]; managementPassword               = nil;
+
     [super dealloc];
 }
 
@@ -1617,14 +1618,16 @@ TBPROPERTY(          NSMutableArray *,         messagesIfConnectionFails,       
 						  ];
 	[self addToLog: logText];
 	
-	NSMutableArray * escapedArguments = [NSMutableArray arrayWithCapacity:[argumentsUsedToStartOpenvpnstart count]];
+    // Log the command used to launch openvpnstart, but don't show the management interface password
+    NSMutableString * escapedArguments = [NSMutableString stringWithCapacity: 1000];
 	unsigned i;
-	for (i=0; i<[argumentsUsedToStartOpenvpnstart count]; i++) {
-		[escapedArguments addObject: [[[argumentsUsedToStartOpenvpnstart objectAtIndex: i] componentsSeparatedByString: @" "] componentsJoinedByString: @"\\ "]];
+	for (i=0; i<([argumentsUsedToStartOpenvpnstart count]-1); i++) {
+		[escapedArguments appendString: [[[argumentsUsedToStartOpenvpnstart objectAtIndex: i]
+                                       componentsSeparatedByString: @" "]
+                                      componentsJoinedByString: @"\\ "]];
 	}
-	
-	[self addToLog: [NSString stringWithFormat: @"openvpnstart %@",
-					 [escapedArguments componentsJoinedByString: @" "]]];
+    [escapedArguments appendString: @" <password>"];
+	[self addToLog: [NSString stringWithFormat: @"openvpnstart %@", escapedArguments]];
 	
 	unsigned bitMask = [[argumentsUsedToStartOpenvpnstart objectAtIndex: 7] unsignedIntValue];
 	if (  (loadedOurTap = (bitMask & OPENVPNSTART_OUR_TAP_KEXT) == OPENVPNSTART_OUR_TAP_KEXT)  ) {
@@ -1843,6 +1846,8 @@ static pthread_mutex_t areConnectingMutex = PTHREAD_MUTEX_INITIALIZER;
 	[self setDynamicChallengePrompt:   nil];
 	[self setDynamicChallengeFlags:    nil];
 
+    [self createAndStoreManagementPassword];
+    
 	disconnectWhenStateChanges = FALSE;
     
     NSString * oldRequestedState = [self requestedState];
@@ -2900,14 +2905,14 @@ static pthread_mutex_t areConnectingMutex = PTHREAD_MUTEX_INITIALIZER;
     NSString * ourOpenVPNVersion = [[gMC openvpnVersionNames] objectAtIndex: finalOpenvpnIx];
 
     NSArray * args = [NSArray arrayWithObjects:
-                      @"start", [[lastPartOfPath(cfgPath) copy] autorelease], portString, useDNSArg, skipScrSec, altCfgLoc, noMonitor, bitMaskString, leasewatchOptions, ourOpenVPNVersion, nil];
+                      @"start", [[lastPartOfPath(cfgPath) copy] autorelease], portString, useDNSArg, skipScrSec, altCfgLoc, noMonitor, bitMaskString, leasewatchOptions, ourOpenVPNVersion, [self managementPassword], nil];
 
     // IF THE NUMBER OF ARGUMENTS CHANGES:
     //    (1) Modify openvpnstart to use the new arguments
     //    (2) Change OPENVPNSTART_MAX_ARGC in defines.h to the maximum 'argc' for openvpnstart
     //        (That is, change it to one more than the number of entries in 'args' (because the path to openvpnstart is also an argument)
     //    (3) Change the constant integer in the next line to the same number
-#if 11 != OPENVPNSTART_MAX_ARGC
+#if 12 != OPENVPNSTART_MAX_ARGC
     #error "OPENVPNSTART_MAX_ARGC is not correct. It must be 1 more than the count of the 'args' array"
 #endif
     
@@ -3562,19 +3567,20 @@ static pthread_mutex_t lastStateMutex = PTHREAD_MUTEX_INITIALIZER;
 
     NSParameterAssert(socket == managementSocket);
     
-	if (  tryingToHookup  ) TBLog(@"DB-HU", @"['%@'] netsocketConnected: invoked ; sending commands to port %lu", displayName, (unsigned long)[managementSocket remotePort])
-	
+    if (  tryingToHookup  ) {
+        TBLog(@"DB-HU", @"['%@'] netsocketConnected: invoked ; sending commands to port %lu", displayName, (unsigned long)[managementSocket remotePort])
+    }
+    
     TBLog(@"DB-ALL", @"Tunnelblick connected to management interface on port %d.", [managementSocket remotePort]);
     
-    NSString * theMipName = mipName();
-	if (  theMipName  ) {
-		NSString * mipString = [theMipName stringByAppendingString: @"\r\n"];
+    NSString * password = [self managementPassword];
+    if (  password  ) {
 		NSString * authRetryCommand = (  [self forceAuthRetryInteract]
 									   ? @"auth-retry interact\r\n"
 									   : nil);
 		
 		NS_DURING {
-			[managementSocket writeString: mipString					encoding: NSASCIIStringEncoding];
+			[managementSocket writeString: [password stringByAppendingString: @"\r\n"] encoding: NSASCIIStringEncoding];
 			[managementSocket writeString: @"pid\r\n"					encoding: NSASCIIStringEncoding];
 			if (  authRetryCommand  ) {
 				[managementSocket writeString: authRetryCommand			encoding: NSASCIIStringEncoding];
@@ -3587,7 +3593,7 @@ static pthread_mutex_t lastStateMutex = PTHREAD_MUTEX_INITIALIZER;
 		}
 		NS_ENDHANDLER
 	} else {
-		NSLog(@"Unable to find .mip file");
+		NSLog(@"Unable to find management interface password");
 	}
 }
 
@@ -5528,6 +5534,72 @@ static pthread_mutex_t lastStateMutex = PTHREAD_MUTEX_INITIALIZER;
 
 	return [[myAuthAgent retain] autorelease];
 }
+
+NSString * getStringOf64RandomCharacters(void) {
+    
+    // Returns a 64 character long string composed of random characters in the range 'a' through 'p'
+    // (A simple encoding of 32 bytes of random data.)
+    
+    // Get 32 random bytes
+    const char bytes[32];
+    int result = SecRandomCopyBytes(kSecRandomDefault, sizeof(bytes), (void *)bytes);
+    if (  result != errSecSuccess  ) {
+        NSLog(@"Unable to obtain data for .mip");
+        return @"";
+    }
+    
+    // Convert each 4-bit nibble to a character from 'a' through 'p'
+    NSMutableString * outString = [[[NSMutableString alloc] initWithCapacity: 2 * sizeof(bytes)] autorelease];
+    NSUInteger ix;
+    for (  ix=0; ix<sizeof(bytes); ix++) {
+        char ch = bytes[ix];
+        char ch1 = (ch & 0xF) + 'a';
+        char ch2 = ((ch >> 4) & 0xF) + 'a';
+        [outString appendFormat: @"%c%c", ch1, ch2];
+    }
+    return [NSString stringWithString: outString];
+}
+
+-(void) createAndStoreManagementPassword {
+    
+    // Creates a password for the management interface for this configuration,
+    // stores it in a class variable for fast access from this launch of Tunnelblick
+    // and
+    // stores it in the Keychain for access when Tunnelblick has been relaunched.
+    
+    NSString * password = getStringOf64RandomCharacters();
+    
+    [self setManagementPassword: password];
+    
+    KeyChain * kc = [[KeyChain alloc] initWithService: @"Tunnelblick Management Password" withAccountName: [self displayName]];
+    [kc deletePassword];
+    [kc setPassword: password];
+    [kc release];
+}
+
+-(NSString *) managementPassword {
+    
+    // Returns the class variable if it is available, otherwise returns the copy stored in the Keychain
+    
+    NSString * password = [[managementPassword retain] autorelease];
+    
+    if (  ! password  ) {
+        KeyChain * kc = [[[KeyChain alloc] initWithService: @"Tunnelblick Management Password"
+                                           withAccountName: [self displayName]]
+                         autorelease];
+        password = [kc password];
+    }
+
+    if (  ! password  ) {
+        NSLog(@"Unable to obtain management interface password from class variable or from the Keychain");
+        return @"";
+    }
+    
+    return password;
+}
+
+TBSYNTHESIZE_OBJECT_SET(        NSString *,               managementPassword, setManagementPassword)
+
 TBSYNTHESIZE_OBJECT_GET(retain, StatusWindowController *, statusScreen)
 
 TBSYNTHESIZE_OBJECT_SET(		NSString *,				  sanitizedConfigurationFileContents, setSanitizedConfigurationFileContents)

tunnelblick/defines.h
@@ -32,7 +32,7 @@
 #define DISPLAY_NAME_LENGTH_MAX 512
 
 // The maximum 'argc' for openvpnstart
-#define OPENVPNSTART_MAX_ARGC 11
+#define OPENVPNSTART_MAX_ARGC 12
 
 // The "admin" and "staff" group IDs
 #define ADMIN_GROUP_ID 80
@@ -117,6 +117,8 @@
 #define L_AS_T_USERS  @"/Library/Application Support/Tunnelblick/Users"
 
 #define L_AS_T_TBLKS  @"/Library/Application Support/Tunnelblick/Tblks"
+
+#define L_AS_T_MIPS  @"/Library/Application Support/Tunnelblick/Mips"
 
 #define L_AS_T_OPENVPN  @"/Library/Application Support/Tunnelblick/Openvpn"
 

tunnelblick/installer.m
@@ -426,31 +426,6 @@ BOOL removeQuarantineBit(void) {
     return TRUE;
 }
 
-NSString * getStringOf40RandomCharacters(void) {
-	
-	// Returns a 40 character long string composed of random characters in the range 'a' through 'p'
-	// (A simple encoding of 20 bytes of random data.)
-	
-	// Get 20 random bytes
-	const char bytes[20];
-	int result = SecRandomCopyBytes(kSecRandomDefault, sizeof(bytes), (void *)bytes);
-	if (  result != errSecSuccess  ) {
-		appendLog(@"Unable to obtain data for .mip");
-		errorExit();
-	}
-	
-	// Convert each 4-bit nibble to a character from 'a' through 'p'
-	NSMutableString * outString = [[[NSMutableString alloc] initWithCapacity: 2 * sizeof(bytes)] autorelease];
-	NSUInteger ix;
-	for (  ix=0; ix<sizeof(bytes); ix++) {
-		char ch = bytes[ix];
-		char ch1 = (ch & 0xF) + 'a';
-		char ch2 = ((ch >> 4) & 0xF) + 'a';
-		[outString appendFormat: @"%c%c", ch1, ch2];
-	}
-	return outString;
-}
-
 /* DISABLED BECAUSE THIS IS NOT AVAILABLE ON 10.4 and 10.5
  *
  * UPDATE 2016-07-03: SMJobSubmit is now deprecated, but we are leaving the code in so that if 'launchctl load' stops working, we can try using it
@@ -1181,6 +1156,11 @@ void doInitialWork(BOOL updateKexts) {
 		errorExit();
 	}
 	
+    if (  ! createDirWithPermissionAndOwnership(L_AS_T_MIPS,
+                                                PERMS_SECURED_FOLDER, 0, 0)  ) {
+        errorExit();
+    }
+    
 	if (  ! createDirWithPermissionAndOwnership(L_AS_T_EXPECT_DISCONNECT_FOLDER_PATH,
 												PERMS_SECURED_FOLDER, 0, 0)  ) {
 		errorExit();
@@ -1208,38 +1188,21 @@ void doInitialWork(BOOL updateKexts) {
 		secureOpenvpnBinariesFolder(L_AS_T_OPENVPN);
 	}
 
-	// Create the .mip file owned by root with 0600 permissions in L_AS_T if it doesn't already exist
-	NSDirectoryEnumerator  * dirEnum = [gFileMgr enumeratorAtPath: L_AS_T];
+	// Delete *.mip files (used before using the Mips folder)
+    NSDirectoryEnumerator  * dirEnum = [gFileMgr enumeratorAtPath: L_AS_T];
 	NSString * fileName;
 	while (  (fileName = [dirEnum nextObject])  ) {
 		[dirEnum skipDescendants];
 		if (  [fileName hasSuffix: @".mip"]  ) {
-			break;
+            NSString * fullPath = [L_AS_T stringByAppendingPathComponent: fileName];
+            if (  [gFileMgr tbRemoveFileAtPath: fullPath handler: nil]  ) {
+                appendLog([NSString stringWithFormat: @"Deleted obsolete file '%@'", fullPath]);
+            } else {
+                appendLog([NSString stringWithFormat: @"Cannot delete obsolete file '%@'", fullPath]);
+            }
 		}
 	}
-	if (  ! fileName) {
-		// Get 40 random characters A-P as a filename for the .mip file
-		NSString * name = getStringOf40RandomCharacters();
-		NSString * path = [L_AS_T stringByAppendingPathComponent: [name stringByAppendingString: @".mip"]];
-		NSString * contents = [name stringByAppendingString: @"\n"];
-		NSData * contentsAsData = [NSData dataWithBytes: [contents cStringUsingEncoding: NSASCIIStringEncoding] length: [contents length]];
-		if (  contentsAsData == NULL  ) {
-			appendLog(@"Unable to create .mip because can't get dataWithBytes with NSASCIIStringEncoding");
-			errorExit();
-		}
-		NSDictionary * attributes = [NSDictionary dictionaryWithObjectsAndKeys:
-									 [NSNumber numberWithInt: 0], NSFileOwnerAccountID,
-									 [NSNumber numberWithInt: 0], NSFileGroupOwnerAccountID,
-									 [NSNumber numberWithInt: PERMS_SECURED_ROOT_RO], NSFilePosixPermissions,
-									 nil];
-		if (  ! [gFileMgr createFileAtPath: path contents: contentsAsData attributes: attributes] ) {
-			appendLog(@"Unable to create .mip");
-			errorExit();
-		}
-		
-		appendLog(@"Created .mip");
-	}
-	
+    
 	NSString * userL_AS_T_Path= [[[NSHomeDirectory()
 								   stringByAppendingPathComponent: @"Library"]
 								  stringByAppendingPathComponent: @"Application Support"]

commit cdf08dcd3cc066d478f7fba0f19abe2739280f52

Fix problem connecting configurations in subfolders
5a21c383 introduced a bug: the path for a .mip file for a configuration in a subfolder included the subfolder's path, which didn't exist in the Mips folder. This replaces the "/" characters in the configuration's path with "-s" so all .mip files a placed directly in the Mips folder.

tunnelblick/tunnelblick-helper.m
@@ -914,7 +914,8 @@ NSString * newTemporaryDirectoryPathInTunnelblickHelper(void) {
 NSString * managementPasswordFilePath(NSString * configName) {
     
     if (  configName  ) {
-        NSString * name = [configName stringByAppendingPathExtension: @"mip"];
+        NSMutableString * name = [[[configName stringByAppendingPathExtension: @"mip"] mutableCopy] autorelease];
+        [name replaceOccurrencesOfString: @"/" withString: @"-S" options: 0 range: NSMakeRange(0, name.length)];
         NSString * path = [L_AS_T_MIPS stringByAppendingPathComponent: name];
         return path;
     }