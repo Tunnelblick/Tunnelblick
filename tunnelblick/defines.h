ec5847d20a6e3d0b45da205067b11ea3cd9c6165
Tunnelblick developer <jkbullard@gmail.com>
2023-05-02 08:06:53 -0400

    Cosmetic: Update defines.h copyright date

--- tunnelblick/defines.h
+++ tunnelblick/defines.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 Jonathan K. Bullard. All rights reserved.
+ * Copyright 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2023 Jonathan K. Bullard. All rights reserved.
  *
  *  This file is part of Tunnelblick.
  *
