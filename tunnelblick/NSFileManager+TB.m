dbeea0f6bfaa5ae1e111d7fca8f4e5603ef37c9d
Tunnelblick developer <jkbullard@gmail.com>
2024-03-30 11:31:31 -0400

    Modernize NSFileManager+TB

--- tunnelblick/NSFileManager+TB.m
+++ tunnelblick/NSFileManager+TB.m
@@ -30,192 +30,188 @@
 
 #import "NSFileManager+TB.h"
 
-id fmCallStack(void) {
-    
-    return (  [NSThread respondsToSelector: @selector(callStackSymbols)]
-            ? (id) [NSThread callStackSymbols]
-            : (id) @"not available");
-}
+#import "defines.h"
+
 void appendLog(NSString * errMsg);
 
 @implementation NSFileManager (TB)
 
--(BOOL) tbChangeFileAttributes:(NSDictionary *)attributes atPath:(NSString *)path {
-    
-    if (  [self respondsToSelector:@selector (setAttributes:ofItemAtPath:error:)]  ) {
-        NSError * err = nil;
-        BOOL answer = [self setAttributes:attributes ofItemAtPath:path error: &err];
-        if (  ! answer  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from setAttributes: %@ ofItemAtPath: %@; Error was %@; stack trace: %@", attributes, path, err, fmCallStack()];
-            appendLog(errMsg);
-        }
-        return answer;
+-(BOOL) tbChangeFileAttributes: (NSDictionary *) attributes
+                        atPath: (NSString * )    path {
+
+    NSError * err = nil;
+    if (  ! [self setAttributes: attributes ofItemAtPath: path error: &err]  ) {
+        NSString * errMsg = [NSString stringWithFormat:
+                             @"Error returned from setAttributes: %@ ofItemAtPath: '%@'; Error was %@; stack trace: %@",
+                             attributes, path, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+        return NO;
     }
-    
-    appendLog(@"No implementation for changeFileAttributes:atPath:");
-    return NO;
+
+    return YES;
 }
 
 
--(BOOL) tbCopyPath:(NSString *)source toPath:(NSString *)destination handler:(id)handler {
-    
+-(BOOL) tbCopyPath: (NSString *) source
+            toPath: (NSString *) destination
+           handler: (id)         handler {
+
     (void) handler;
     
-    if (  [self respondsToSelector:@selector (copyItemAtPath:toPath:error:)]  ) {
-		NSError * err = nil;
-        BOOL answer = [self copyItemAtPath:source toPath:destination error: &err];
-        if (  ! answer  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from copyItemAtPath: %@ toPath: %@; Error was %@; stack trace: %@", source, destination, err, fmCallStack()];
-            appendLog(errMsg);
-        }
-        return answer;
+    NSError * err = nil;
+    if (  ! [self copyItemAtPath:source toPath:destination error: &err]  ) {
+        NSString * errMsg = [NSString stringWithFormat: 
+                             @"Error returned from copyItemAtPath: '%@' toPath: '%@'; Error was %@; stack trace: %@",
+                             source, destination, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+        return NO;
     }
-    
-    appendLog(@"No implementation for copyPath:toPath:handler:");
-    return NO;
+
+    return YES;
 }
 
 
--(BOOL) tbCreateDirectoryAtPath:(NSString *)path
-	withIntermediateDirectories:(BOOL)withIntermediateDirectories
-					 attributes:(NSDictionary *)attributes {
+-(BOOL) tbCreateDirectoryAtPath: (NSString *)     path
+	withIntermediateDirectories: (BOOL)           withIntermediateDirectories
+					 attributes: (NSDictionary *) attributes {
 
-	if (  [self respondsToSelector:@selector (createDirectoryAtPath:withIntermediateDirectories:attributes:error:)]  ) {
-		NSError * err = nil;
-		BOOL answer = [self createDirectoryAtPath:path withIntermediateDirectories:withIntermediateDirectories attributes:attributes error: &err];
-		if (  ! answer  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from createDirectoryAtPath: %@ withIntermediateDirectories: NO attributes: %@; Error was %@; stack trace: %@", path, attributes, err, fmCallStack()];
-			appendLog(errMsg);
-		}
-		return answer;
-	}
-
-	appendLog(@"No implementation for createDirectoryAtPath:withIntermediateDirectories:attributes:");
-	return NO;
-}
-
--(BOOL) tbCreateSymbolicLinkAtPath:(NSString *)path pathContent:(NSString *)otherPath {
-    
-    if (  [self respondsToSelector:@selector (createSymbolicLinkAtPath:withDestinationPath:error:)]  ) {
-        NSError * err = nil;
-        BOOL answer = [self createSymbolicLinkAtPath:path withDestinationPath:otherPath error: &err];
-        if (  ! answer  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from createSymbolicLinkAtPath: %@ withDestinationPath: %@; Error was %@; stack trace: %@", path, otherPath, err, fmCallStack()];
-            appendLog(errMsg);
-        }
-        return answer;
+	NSError * err = nil;
+    if (  ! [self createDirectoryAtPath: path withIntermediateDirectories: withIntermediateDirectories attributes: attributes error: &err]  ) {
+        NSString * errMsg = [NSString stringWithFormat:
+                             @"Error returned from createDirectoryAtPath: '%@' withIntermediateDirectories: %s attributes: %@; Error was %@; stack trace: %@",
+                             path, CSTRING_FROM_BOOL(withIntermediateDirectories), attributes, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+        return NO;
     }
-    
-    appendLog(@"No implementation for createSymbolicLinkAtPath:pathContent:");
-    return NO;
+
+    return YES;
 }
 
+-(BOOL) tbCreateSymbolicLinkAtPath: (NSString *) path
+                       pathContent: (NSString *) otherPath {
 
--(NSArray *) tbDirectoryContentsAtPath:(NSString *)path {
-    
-    if (  [self respondsToSelector:@selector (contentsOfDirectoryAtPath:error:)]  ) {
-        NSError * err = nil;
-        NSArray * answer = [self contentsOfDirectoryAtPath:path error: &err];
-        if (  ! answer  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from contentsOfDirectoryAtPath: %@; Error was %@; stack trace: %@", path, err, fmCallStack()];
-            appendLog(errMsg);
-        }
-        return answer;
+    NSError * err = nil;
+    if (  ! [self createSymbolicLinkAtPath: path withDestinationPath: otherPath error: &err]  ) {
+        NSString * errMsg = [NSString stringWithFormat:
+                             @"Error returned from createSymbolicLinkAtPath: '%@' withDestinationPath: '%@'; Error was %@; stack trace: %@",
+                             path, otherPath, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+        return NO;
     }
-    
-    appendLog(@"No implementation for directoryContentsAtPath:");
-    return nil;
+
+    return YES;
 }
 
 
--(NSDictionary *) tbFileAttributesAtPath:(NSString *)path traverseLink:(BOOL)flag {
-    
-    if (  [self respondsToSelector:@selector (attributesOfItemAtPath:error:)]  ) {
-        NSError * err = nil;
-        NSDictionary * attributes = [self attributesOfItemAtPath:path error: &err];
+-(NSArray *) tbDirectoryContentsAtPath:(NSString *) path {
+
+    NSError * err = nil;
+    NSArray * answer = [self contentsOfDirectoryAtPath:path error: &err];
+    if (  ! answer  ) {
+        NSString * errMsg = [NSString stringWithFormat: 
+                             @"Error returned from contentsOfDirectoryAtPath: '%@'; Error was %@; stack trace: %@",
+                             path, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+    }
+
+    return answer;
+}
+
+
+-(NSDictionary *) tbFileAttributesAtPath: (NSString *) path
+                            traverseLink: (BOOL)       flag {
+
+    NSError * err = nil;
+    NSDictionary * attributes = [self attributesOfItemAtPath:path error: &err];
+    if (  ! attributes  ) {
+        NSString * errMsg = [NSString stringWithFormat:
+                             @"Error returned from attributesOfItemAtPath: '%@';\nError was %@; stack trace: %@",
+                             path, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+        return nil;
+    }
+
+    unsigned int counter = 0;
+    NSString * realPath = nil;
+    NSString * newPath  = [[path copy] autorelease];
+    while (   flag
+           && ( counter++ < 10 )
+           && [[attributes objectForKey: NSFileType] isEqualToString: NSFileTypeSymbolicLink] ) {
+        realPath = [self tbPathContentOfSymbolicLinkAtPath: newPath];
+        if (  ! realPath  ) {
+            return nil;
+        }
+        if (  ! [realPath hasPrefix: @"/"]  ) {
+            realPath = [[newPath stringByDeletingLastPathComponent] stringByAppendingPathComponent: realPath];
+        }
+        attributes = [self attributesOfItemAtPath:realPath error: &err];
         if (  ! attributes  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from attributesOfItemAtPath: %@;\nError was %@; stack trace: %@", path, err, fmCallStack()];
+            NSString * errMsg = [NSString stringWithFormat:
+                                 @"Error returned from attributesOfItemAtPath: '%@';\nOriginal path was' %@'\nLatest path = '%@';\nError was %@; stack trace: %@",
+                                 realPath, path, newPath, err, [NSThread callStackSymbols]];
             appendLog(errMsg);
             return nil;
-       }
-        unsigned int counter = 0;
-		NSString * realPath = nil;
-		NSString * newPath  = [[path copy] autorelease];
-        while (   flag
-			   && ( counter++ < 10 )
-               && [[attributes objectForKey: NSFileType] isEqualToString: NSFileTypeSymbolicLink] ) {
-            realPath = [self tbPathContentOfSymbolicLinkAtPath: newPath];
-			if (  ! realPath  ) {
-				NSString * errMsg = [NSString stringWithFormat: @"Error returned from tbPathContentOfSymbolicLinkAtPath: %@;\nOriginal path = %@; stack trace: %@", newPath, path, fmCallStack()];
-                appendLog(errMsg);
-				return nil;
-			}
-			if (  ! [realPath hasPrefix: @"/"]  ) {
-				realPath = [[newPath stringByDeletingLastPathComponent] stringByAppendingPathComponent: realPath];
-			}
-            attributes = [self attributesOfItemAtPath:realPath error: &err];
-            if (  ! attributes  ) {
-				NSString * errMsg = [NSString stringWithFormat: @"Error returned from attributesOfItemAtPath: %@;\nOriginal path was %@\nLatest path = %@;\nError was %@; stack trace: %@", realPath, path, newPath, err, fmCallStack()];
-                appendLog(errMsg);
-                return nil;
-            }
-            newPath = [[realPath copy] autorelease];
         }
-		if (  counter >= 10  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"tbFileAttributesAtPath detected a symlink loop.\nOriginal path was %@\nLast \"Real\" path was %@, attributes = %@; stack trace: %@", path, realPath, attributes, fmCallStack()];
-            appendLog(errMsg);
-		}
-        return attributes;
+
+        newPath = [[realPath copy] autorelease];
     }
-    
-    appendLog(@"No implementation for fileAttributesAtPath:traverseLink:");
-    return nil;
+
+    if (  counter >= 10  ) {
+        NSString * errMsg = [NSString stringWithFormat:
+                             @"tbFileAttributesAtPath detected a symlink loop.\nOriginal path was '%@'\nLast \"Real\" path was '%@', attributes = %@; stack trace: %@",
+                             path, realPath, attributes, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+    }
+
+    return attributes;
 }
 
 
--(BOOL) tbMovePath:(NSString *)source toPath:(NSString *)destination handler:(id)handler {
-    
+-(BOOL) tbMovePath: (NSString *) source
+            toPath: (NSString *) destination
+           handler: (id)         handler {
+
     (void) handler;
     
-    if (  [self respondsToSelector:@selector (moveItemAtPath:toPath:error:)]  ) {
-        // The apple docs are vague about what this does compared to movePath:toPath:handler:.
-        // So we hope it works the same. (Regarding same-device moves, for example.)
-        NSError * err = nil;
-        BOOL answer = [self moveItemAtPath:source toPath:destination error: &err];
-        if (  ! answer  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from moveItemAtPath: %@ toPath: %@; Error was %@; stack trace: %@", source, destination, err, fmCallStack()];
-            appendLog(errMsg);
-        }
-        return answer;
+    NSError * err = nil;
+    if (  ! [self moveItemAtPath: source toPath: destination error: &err]  ) {
+        NSString * errMsg = [NSString stringWithFormat:
+                             @"Error returned from moveItemAtPath: '%@' toPath: '%@'; Error was %@; stack trace: %@",
+                             source, destination, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+        return NO;
     }
-    
-    appendLog(@"No implementation for movePath:toPath:handler:");
-    return NO;
+
+    return YES;
 }
 
 
--(BOOL) tbRemoveFileAtPath:(NSString *)path handler:(id)handler {
-    
+-(BOOL) tbRemoveFileAtPath: (NSString *) path
+                   handler: (id) handler {
+
     (void) handler;
     
-    if (  [self respondsToSelector:@selector (removeItemAtPath:error:)]  ) {
-        NSError * err = nil;
-        BOOL answer = [self removeItemAtPath:path error: &err];
-        if (  ! answer  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from removeItemAtPath: %@; Error was %@; stack trace: %@", path, err, fmCallStack()];
-            appendLog(errMsg);
-        }
-        return answer;
+    NSError * err = nil;
+    if (  ! [self removeItemAtPath:path error: &err]  ) {
+        NSString * errMsg = [NSString stringWithFormat: 
+                             @"Error returned from removeItemAtPath: '%@'; Error was %@; stack trace: %@",
+                             path, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
+        return NO;
     }
-    
-    appendLog(@"No implementation for removeFileAtPath:handler:");
-    return NO;
+
+    return YES;
 }
 
 -(BOOL) tbRemovePathIfItExists: (NSString *) path {
     
     if (  [self fileExistsAtPath: path]  ) {
-        if (  ! [self tbRemoveFileAtPath: path handler: nil]  ) {
+        NSError * err = nil;
+        if (  ! [self removeItemAtPath: path error: &err]  ) {
+            NSString * errMsg = [NSString stringWithFormat:
+                                 @"remove '%@' failed; error was '%@'; stack trace: %@",
+                                 path, err, [NSThread callStackSymbols]];
+            appendLog(errMsg);
             return NO;
         }
     }
@@ -223,15 +219,14 @@ void appendLog(NSString * errMsg);
     return YES;
 }
 
--(BOOL) tbForceRenamePath: (NSString *) sourcePath toPath: (NSString *) targetPath {
-    
-	if (  ! [self tbRemovePathIfItExists: targetPath]  ) {
-        return NO;
-	}
-	
+-(BOOL) tbForceRenamePath: (NSString *) sourcePath
+                   toPath: (NSString *) targetPath {
+
     int status = rename([sourcePath fileSystemRepresentation], [targetPath fileSystemRepresentation]);
     if (  status != 0  ) {
-        NSString * errMsg = [NSString stringWithFormat: @"rename(%@,%@) failed; status = %ld; errno = %ld; error was '%s'; stack trace: %@", sourcePath, targetPath, (long)status, (long)errno, strerror(errno), fmCallStack()];
+        NSString * errMsg = [NSString stringWithFormat:
+                             @"rename('%@','%@') failed; status = %ld; errno = %ld; error was '%s'; stack trace: %@",
+                             sourcePath, targetPath, (long)status, (long)errno, strerror(errno), [NSThread callStackSymbols]];
 		appendLog(errMsg);
         return NO;
     }
@@ -240,20 +235,18 @@ void appendLog(NSString * errMsg);
 }
 
 
--(NSString *) tbPathContentOfSymbolicLinkAtPath:(NSString *)path {
-    
-    if (  [self respondsToSelector:@selector (destinationOfSymbolicLinkAtPath:error:)]  ) {
-		NSError * err = nil;
-		NSString * answer = [self destinationOfSymbolicLinkAtPath:path error: &err];
-		if (  ! answer  ) {
-			NSString * errMsg = [NSString stringWithFormat: @"Error returned from destinationOfSymbolicLinkAtPath: %@; Error was %@; stack trace: %@", path, err, fmCallStack()];
-            appendLog(errMsg);
-		}
-        return answer;
+-(NSString *) tbPathContentOfSymbolicLinkAtPath: (NSString *) path {
+
+    NSError * err = nil;
+    NSString * answer = [self destinationOfSymbolicLinkAtPath:path error: &err];
+    if (  ! answer  ) {
+        NSString * errMsg = [NSString stringWithFormat:
+                             @"Error returned from destinationOfSymbolicLinkAtPath: '%@'; Error was %@; stack trace: %@",
+                             path, err, [NSThread callStackSymbols]];
+        appendLog(errMsg);
     }
-    
-    appendLog(@"No implementation for pathContentOfSymbolicLinkAtPath:");
-    return nil;
+
+    return answer;
 }
 
 @end
