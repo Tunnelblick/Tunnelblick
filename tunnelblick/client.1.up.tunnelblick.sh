Tunnelblick/Tunnelblick

commit 540a841a09e6f5577f9adc891440562264d286ab

Prevent injection attacks in client.up scripts

tunnelblick/client.1.up.tunnelblick.sh
@@ -4,6 +4,145 @@ trap "" HUP
 trap "" INT
 export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
 
+configuration_display_name() {
+
+    local display_name
+
+    display_name="${TUNNELBLICK_CONFIG_FOLDER/.tblk\/Contents\/Resources/}"                                                 # Strip suffix from configuration path
+    if [ "${display_name/\/Library\/Application Support\/Tunnelblick\/Users\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Library\/Application Support\/Tunnelblick\/Users\//}"                                # Strip prefix up to username/ from private configuration path
+        display_name="${display_name#*\/}"                                                                                  # Strip username/ from private config"
+    elif [ "${display_name/\/Library\/Application Support\/Tunnelblick\/Shared\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Library\/Application Support\/Tunnelblick\/Shared\//}"                               # Strip prefix from shared configuration path
+    elif [ "${display_name/\/Applications\/Tunnelblick.app\/Contents\/Resources\/Deploy\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Applications\/Tunnelblick.app\/Contents\/Resources\/Deploy\//}"                      # Strip prefix from Deployed configuration path
+    else
+        echo "Error: Unable to determine configuration's display name from configuration path '$TUNNELBLICK_CONFIG_FOLDER'"
+        exit 1
+    fi
+
+    echo "$display_name"
+}
+
+disconnect_and_exit() {
+
+    local display_name
+    display_name="$( configuration_display_name )"
+
+    echo "Disconnecting '$display_name'"
+
+    osascript -e "tell application \"/Applications/Tunnelblick.app\"" -e "disconnect \"$display_name\"" -e "end tell"
+
+    exit 1
+}
+
+exit_if_suspicious_domain_name() {
+
+    # @param String string - Content to test
+    #
+    # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
+    #
+    # This is a very loose test, it exits after outputting an error message if a domain name
+    # contains characters that don't belong in a domain name.
+    #
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    #
+    # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
+    # names that are longer than 63 characters.
+    #
+    # Allows Unicode characters, and allows punycode, which consists of the same characters as a pure ASCII domain name.
+
+    local str
+    local i
+    local val
+
+    str="$1"
+
+    for (( i=0; i<${#str}; i++ )); do
+        ch="${str:$i:1}"
+        val=$(printf '%d' "'$ch")
+
+        if [ "$val" -ge  65 ] \
+        && [ "$val" -le  90 ] ; then
+            continue; # A-Z
+        fi
+
+        if [ "$val" -ge  97 ] \
+        && [ "$val" -le 122 ] ; then
+            continue; # a-z
+        fi
+
+        if [ "$val" -ge 48 ] \
+        && [ "$val" -le 57 ] ; then
+            continue; # 0-9
+        fi
+
+        if [ "$val" -ge 127 ] \
+        || [ "$val" -le 0 ] ; then
+            continue; # > 0x80
+        fi
+
+        if [ "$ch" = "." ] \
+        || [ "$ch" = "-" ] ; then # . or -
+            continue;
+        fi
+
+        echo "Error: Disconnecting because of a suspicious domain name: '$str'"
+        disconnect_and_exit
+    done
+}
+
+exit_if_suspicious_ip_address() {
+
+    # @param String string - Content to test
+    #
+    # Prevent script injection attacks from an IP address supplied by the OpenVPN server.
+    #
+    # This is a very loose test, it exits after outputting an error message if an IP address
+    # contains characters that don't belong in an IP address).
+    #
+    # Allows empty string or any sequence of the characters 0-9, A-F, a-f, "." and ":".
+    #
+    # Will allow invalid IP addresses, for example, abc.123.456:...:::::::::::::::: and a.b.c.d.e.f.
+
+    local str
+    local i
+    local val
+
+    str="$1"
+
+    for (( i=0; i<${#str}; i++ )); do
+        ch="${str:$i:1}"
+        val=$(printf '%d' "'$ch")
+
+        # Allow 0-9
+        if [ "$val" -ge 48 ] \
+        && [ "$val" -le 57 ] ; then
+            continue; # 0-9
+        fi
+
+        # Allow A-F
+        if [ "$val" -ge 65 ] \
+        && [ "$val" -le 70 ] ; then
+            continue; # A-F
+        fi
+
+        # Allow a-f
+        if [ "$val" -ge 97 ] \
+        && [ "$val" -le 102 ] ; then
+            continue; # a-f
+        fi
+
+        if [ "$ch" = "." ] \
+        || [ "$ch" = ":" ] ; then
+            continue;
+        fi
+
+        echo "Error: Disconnecting because of a suspicious IP address: '$str'"
+        disconnect_and_exit
+    done
+}
+
 INTERFACE=$1
 IDENTIFYER="org.openvpn.${INTERFACE}"
 
@@ -22,15 +161,18 @@ while vForOptions=foreign_option_$nOptionIndex; [ -n "${!vForOptions}" ]; do
 	{
 	vOptions[nOptionIndex-1]=${!vForOptions}
 	case ${vOptions[nOptionIndex-1]} in
-	*DOMAIN* )
-	domain=${vOptions[nOptionIndex-1]//dhcp-option DOMAIN /}
-	;;
-	*DNS*    )
-	vDNS[nNameServerIndex-1]=${vOptions[nOptionIndex-1]//dhcp-option DNS /}
-	let nNameServerIndex++
-	;;
-esac
-let nOptionIndex++
+        *DOMAIN* )
+            domain=${vOptions[nOptionIndex-1]//dhcp-option DOMAIN /}
+            exit_if_suspicious_domain_name "$domain"
+            ;;
+        *DNS*    )
+            sTempVal=${vOptions[nOptionIndex-1]//dhcp-option DNS /}
+            exit_if_suspicious_ip_address "$sTempVal"
+            vDNS[nNameServerIndex-1]="$sTempVal"
+            let nNameServerIndex++
+            ;;
+    esac
+    let nOptionIndex++
 }
 done
 

tunnelblick/client.2.up.tunnelblick.sh
@@ -70,6 +70,145 @@ trap "" HUP
 trap "" INT
 export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
 
+configuration_display_name() {
+
+    local display_name
+
+    display_name="${TUNNELBLICK_CONFIG_FOLDER/.tblk\/Contents\/Resources/}"                                                 # Strip suffix from configuration path
+    if [ "${display_name/\/Library\/Application Support\/Tunnelblick\/Users\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Library\/Application Support\/Tunnelblick\/Users\//}"                                # Strip prefix up to username/ from private configuration path
+        display_name="${display_name#*\/}"                                                                                  # Strip username/ from private config"
+    elif [ "${display_name/\/Library\/Application Support\/Tunnelblick\/Shared\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Library\/Application Support\/Tunnelblick\/Shared\//}"                               # Strip prefix from shared configuration path
+    elif [ "${display_name/\/Applications\/Tunnelblick.app\/Contents\/Resources\/Deploy\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Applications\/Tunnelblick.app\/Contents\/Resources\/Deploy\//}"                      # Strip prefix from Deployed configuration path
+    else
+        echo "Error: Unable to determine configuration's display name from configuration path '$TUNNELBLICK_CONFIG_FOLDER'"
+        exit 1
+    fi
+
+    echo "$display_name"
+}
+
+disconnect_and_exit() {
+
+    local display_name
+    display_name="$( configuration_display_name )"
+
+    echo "Disconnecting '$display_name'"
+
+    osascript -e "tell application \"/Applications/Tunnelblick.app\"" -e "disconnect \"$display_name\"" -e "end tell"
+
+    exit 1
+}
+
+exit_if_suspicious_domain_name() {
+
+    # @param String string - Content to test
+    #
+    # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
+    #
+    # This is a very loose test, it exits after outputting an error message if a domain name
+    # contains characters that don't belong in a domain name.
+    #
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    #
+    # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
+    # names that are longer than 63 characters.
+    #
+    # Allows Unicode characters, and allows punycode, which consists of the same characters as a pure ASCII domain name.
+
+    local str
+    local i
+    local val
+
+    str="$1"
+
+    for (( i=0; i<${#str}; i++ )); do
+        ch="${str:$i:1}"
+        val=$(printf '%d' "'$ch")
+
+        if [ "$val" -ge  65 ] \
+        && [ "$val" -le  90 ] ; then
+            continue; # A-Z
+        fi
+
+        if [ "$val" -ge  97 ] \
+        && [ "$val" -le 122 ] ; then
+            continue; # a-z
+        fi
+
+        if [ "$val" -ge 48 ] \
+        && [ "$val" -le 57 ] ; then
+            continue; # 0-9
+        fi
+
+        if [ "$val" -ge 127 ] \
+        || [ "$val" -le 0 ] ; then
+            continue; # > 0x80
+        fi
+
+        if [ "$ch" = "." ] \
+        || [ "$ch" = "-" ] ; then # . or -
+            continue;
+        fi
+
+        echo "Error: Disconnecting because of a suspicious domain name: '$str'"
+        disconnect_and_exit
+    done
+}
+
+exit_if_suspicious_ip_address() {
+
+    # @param String string - Content to test
+    #
+    # Prevent script injection attacks from an IP address supplied by the OpenVPN server.
+    #
+    # This is a very loose test, it exits after outputting an error message if an IP address
+    # contains characters that don't belong in an IP address).
+    #
+    # Allows empty string or any sequence of the characters 0-9, A-F, a-f, "." and ":".
+    #
+    # Will allow invalid IP addresses, for example, abc.123.456:...:::::::::::::::: and a.b.c.d.e.f.
+
+    local str
+    local i
+    local val
+
+    str="$1"
+
+    for (( i=0; i<${#str}; i++ )); do
+        ch="${str:$i:1}"
+        val=$(printf '%d' "'$ch")
+
+        # Allow 0-9
+        if [ "$val" -ge 48 ] \
+        && [ "$val" -le 57 ] ; then
+            continue; # 0-9
+        fi
+
+        # Allow A-F
+        if [ "$val" -ge 65 ] \
+        && [ "$val" -le 70 ] ; then
+            continue; # A-F
+        fi
+
+        # Allow a-f
+        if [ "$val" -ge 97 ] \
+        && [ "$val" -le 102 ] ; then
+            continue; # a-f
+        fi
+
+        if [ "$ch" = "." ] \
+        || [ "$ch" = ":" ] ; then
+            continue;
+        fi
+
+        echo "Error: Disconnecting because of a suspicious IP address: '$str'"
+        disconnect_and_exit
+    done
+}
+
 # Process optional arguments (if any) for the script
 # Each one begins with a "-"
 # They come from Tunnelblick, and come first, before the OpenVPN arguments
@@ -202,8 +341,16 @@ else
      do
          #echo "$n - $o ($i, $j)"
          case $o in
-             'dhcp-option DNS '*)        dns[i++]=${o/dhcp-option DNS /};;
-             'dhcp-option DOMAIN '*)     domain[j++]=${o/dhcp-option DOMAIN /} ;;
+             'dhcp-option DNS '*)
+				sTempVal=${o/dhcp-option DNS /}
+                exit_if_suspicious_ip_address "$sTempVal"
+				dns[i++]="$sTempVal"
+                ;;
+             'dhcp-option DOMAIN '*)
+				sTempVal=${o/dhcp-option DOMAIN /}
+                exit_if_suspicious_domain_name "$sTempVal"
+                domain[j++]="$sTempVal"
+                ;;
          esac;
          let n++
      done

tunnelblick/client.3.up.tunnelblick.sh
@@ -7,6 +7,145 @@ trap "" HUP
 trap "" INT
 export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
 
+configuration_display_name() {
+
+    local display_name
+
+    display_name="${TUNNELBLICK_CONFIG_FOLDER/.tblk\/Contents\/Resources/}"                                                 # Strip suffix from configuration path
+    if [ "${display_name/\/Library\/Application Support\/Tunnelblick\/Users\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Library\/Application Support\/Tunnelblick\/Users\//}"                                # Strip prefix up to username/ from private configuration path
+        display_name="${display_name#*\/}"                                                                                  # Strip username/ from private config"
+    elif [ "${display_name/\/Library\/Application Support\/Tunnelblick\/Shared\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Library\/Application Support\/Tunnelblick\/Shared\//}"                               # Strip prefix from shared configuration path
+    elif [ "${display_name/\/Applications\/Tunnelblick.app\/Contents\/Resources\/Deploy\//}" != "$display_name" ] ; then
+        display_name="${display_name/\/Applications\/Tunnelblick.app\/Contents\/Resources\/Deploy\//}"                      # Strip prefix from Deployed configuration path
+    else
+        echo "Error: Unable to determine configuration's display name from configuration path '$TUNNELBLICK_CONFIG_FOLDER'"
+        exit 1
+    fi
+
+    echo "$display_name"
+}
+
+disconnect_and_exit() {
+
+    local display_name
+    display_name="$( configuration_display_name )"
+
+    echo "Disconnecting '$display_name'"
+
+    osascript -e "tell application \"/Applications/Tunnelblick.app\"" -e "disconnect \"$display_name\"" -e "end tell"
+
+    exit 1
+}
+
+exit_if_suspicious_domain_name() {
+
+    # @param String string - Content to test
+    #
+    # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
+    #
+    # This is a very loose test, it exits after outputting an error message if a domain name
+    # contains characters that don't belong in a domain name.
+    #
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    #
+    # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
+    # names that are longer than 63 characters.
+    #
+    # Allows Unicode characters, and allows punycode, which consists of the same characters as a pure ASCII domain name.
+
+    local str
+    local i
+    local val
+
+    str="$1"
+
+    for (( i=0; i<${#str}; i++ )); do
+        ch="${str:$i:1}"
+        val=$(printf '%d' "'$ch")
+
+        if [ "$val" -ge  65 ] \
+        && [ "$val" -le  90 ] ; then
+            continue; # A-Z
+        fi
+
+        if [ "$val" -ge  97 ] \
+        && [ "$val" -le 122 ] ; then
+            continue; # a-z
+        fi
+
+        if [ "$val" -ge 48 ] \
+        && [ "$val" -le 57 ] ; then
+            continue; # 0-9
+        fi
+
+        if [ "$val" -ge 127 ] \
+        || [ "$val" -le 0 ] ; then
+            continue; # > 0x80
+        fi
+
+        if [ "$ch" = "." ] \
+        || [ "$ch" = "-" ] ; then # . or -
+            continue;
+        fi
+
+        echo "Error: Disconnecting because of a suspicious domain name: '$str'"
+        disconnect_and_exit
+    done
+}
+
+exit_if_suspicious_ip_address() {
+
+    # @param String string - Content to test
+    #
+    # Prevent script injection attacks from an IP address supplied by the OpenVPN server.
+    #
+    # This is a very loose test, it exits after outputting an error message if an IP address
+    # contains characters that don't belong in an IP address).
+    #
+    # Allows empty string or any sequence of the characters 0-9, A-F, a-f, "." and ":".
+    #
+    # Will allow invalid IP addresses, for example, abc.123.456:...:::::::::::::::: and a.b.c.d.e.f.
+
+    local str
+    local i
+    local val
+
+    str="$1"
+
+    for (( i=0; i<${#str}; i++ )); do
+        ch="${str:$i:1}"
+        val=$(printf '%d' "'$ch")
+
+        # Allow 0-9
+        if [ "$val" -ge 48 ] \
+        && [ "$val" -le 57 ] ; then
+            continue; # 0-9
+        fi
+
+        # Allow A-F
+        if [ "$val" -ge 65 ] \
+        && [ "$val" -le 70 ] ; then
+            continue; # A-F
+        fi
+
+        # Allow a-f
+        if [ "$val" -ge 97 ] \
+        && [ "$val" -le 102 ] ; then
+            continue; # a-f
+        fi
+
+        if [ "$ch" = "." ] \
+        || [ "$ch" = ":" ] ; then
+            continue;
+        fi
+
+        echo "Error: Disconnecting because of a suspicious IP address: '$str'"
+        disconnect_and_exit
+    done
+}
+
 # Process optional arguments (if any) for the script
 # Each one begins with a "-"
 # They come from Tunnelblick, and come first, before the OpenVPN arguments
@@ -117,14 +256,19 @@ while vForOptions=foreign_option_$nOptionIndex; [ -n "${!vForOptions}" ]; do
 	case ${vOptions[nOptionIndex-1]} in
 		*DOMAIN* )
 			domain="$(trim "${vOptions[nOptionIndex-1]//dhcp-option DOMAIN /}")"
-			;;
+			exit_if_suspicious_domain_name "$domain"
+            ;;
 		*DNS*    )
-			vDNS[nNameServerIndex-1]="$(trim "${vOptions[nOptionIndex-1]//dhcp-option DNS /}")"
-			let nNameServerIndex++
-			;;
+            sTempVal="$(trim "${vOptions[nOptionIndex-1]//dhcp-option DNS /}")"
+            exit_if_suspicious_ip_address "$sTempVal"
+            vDNS[nNameServerIndex-1]="$sTempVal"
+            let nNameServerIndex++
+            ;;
 		*WINS*    )
-			vWINS[nWINSServerIndex-1]="$(trim "${vOptions[nOptionIndex-1]//dhcp-option WINS /}")"
-			let nWINSServerIndex++
+			sTempVal="$(trim "${vOptions[nOptionIndex-1]//dhcp-option WINS /}")"
+			exit_if_suspicious_ip_address "$sTempVal"
+            vWINS[nWINSServerIndex-1]="$sTempVal"
+            let nWINSServerIndex++
 			;;
 	esac
 	let nOptionIndex++

commit 986035111f1c55ec08fa15fd5457ab1eeb0eea0c

Fix problem with multiple search domains
The problem was introduced in commit 540a841, which prevents script injection attacks in a domain name suppled by the OpenVPN server. That commit did not allow spaces which separate multiple domain names.

tunnelblick/client.1.up.tunnelblick.sh
@@ -43,9 +43,9 @@ exit_if_suspicious_domain_name() {
     # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
     #
     # This is a very loose test, it exits after outputting an error message if a domain name
-    # contains characters that don't belong in a domain name.
+    # contains any characters other than spaces or characters which don't belong in a domain name.
     #
-    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, ".", "-", space, and any Unicode characters.
     #
     # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
     # names that are longer than 63 characters.
@@ -83,7 +83,8 @@ exit_if_suspicious_domain_name() {
         fi
 
         if [ "$ch" = "." ] \
-        || [ "$ch" = "-" ] ; then # . or -
+        || [ "$ch" = "-" ] \
+        || [ "$ch" = " " ] ; then # . or - or space
             continue;
         fi
 

tunnelblick/client.2.up.tunnelblick.sh
@@ -109,9 +109,9 @@ exit_if_suspicious_domain_name() {
     # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
     #
     # This is a very loose test, it exits after outputting an error message if a domain name
-    # contains characters that don't belong in a domain name.
+    # contains any characters other than spaces or characters which don't belong in a domain name.
     #
-    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, ".", "-", space, and any Unicode characters.
     #
     # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
     # names that are longer than 63 characters.
@@ -149,7 +149,8 @@ exit_if_suspicious_domain_name() {
         fi
 
         if [ "$ch" = "." ] \
-        || [ "$ch" = "-" ] ; then # . or -
+        || [ "$ch" = "-" ] \
+        || [ "$ch" = " " ] ; then # . or - or space
             continue;
         fi
 

tunnelblick/client.3.up.tunnelblick.sh
@@ -46,9 +46,9 @@ exit_if_suspicious_domain_name() {
     # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
     #
     # This is a very loose test, it exits after outputting an error message if a domain name
-    # contains characters that don't belong in a domain name.
+    # contains any characters other than spaces or characters which don't belong in a domain name.
     #
-    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, ".", "-", space, and any Unicode characters.
     #
     # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
     # names that are longer than 63 characters.
@@ -86,8 +86,9 @@ exit_if_suspicious_domain_name() {
         fi
 
         if [ "$ch" = "." ] \
-        || [ "$ch" = "-" ] ; then # . or -
-            continue;
+        || [ "$ch" = "-" ] \
+        || [ "$ch" = " " ] ; then # . or - or space
+           continue;
         fi
 
         echo "Error: Disconnecting because of a suspicious domain name: '$str'"

tunnelblick/client.4.up.tunnelblick.sh
@@ -96,9 +96,9 @@ exit_if_suspicious_domain_name() {
     # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
     #
     # This is a very loose test, it exits after outputting an error message if a domain name
-    # contains characters that don't belong in a domain name.
+    # contains any characters other than spaces or characters which don't belong in a domain name.
     #
-    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, ".", "-", space, and any Unicode characters.
     #
     # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
     # names that are longer than 63 characters.
@@ -136,7 +136,8 @@ exit_if_suspicious_domain_name() {
         fi
 
         if [ "$ch" = "." ] \
-        || [ "$ch" = "-" ] ; then # . or -
+        || [ "$ch" = "-" ] \
+        || [ "$ch" = " " ] ; then # . or - or space
             continue;
         fi
 

tunnelblick/client.5.up.tunnelblick.sh
@@ -109,9 +109,9 @@ exit_if_suspicious_domain_name() {
     # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
     #
     # This is a very loose test, it exits after outputting an error message if a domain name
-    # contains characters that don't belong in a domain name.
+    # contains any characters other than spaces or characters which don't belong in a domain name.
     #
-    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, ".", "-", space, and any Unicode characters.
     #
     # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
     # names that are longer than 63 characters.
@@ -149,8 +149,9 @@ exit_if_suspicious_domain_name() {
         fi
 
         if [ "$ch" = "." ] \
-        || [ "$ch" = "-" ] ; then # . or -
-            continue;
+        || [ "$ch" = "-" ] \
+        || [ "$ch" = " " ] ; then # . or - or space
+           continue;
         fi
 
         echo "Error: Disconnecting because of a suspicious domain name: '$str'"

tunnelblick/client.up.tunnelblick.sh
@@ -109,9 +109,9 @@ exit_if_suspicious_domain_name() {
     # Prevent script injection attacks from a domain name supplied by the OpenVPN server.
     #
     # This is a very loose test, it exits after outputting an error message if a domain name
-    # contains characters that don't belong in a domain name.
+    # contains any characters other than spaces or characters which don't belong in a domain name.
     #
-    # Allows empty string or any sequence of characters 0-9, A-F, a-f, "." and "-", and any Unicode characters.
+    # Allows empty string or any sequence of characters 0-9, A-F, a-f, ".", "-", space, and any Unicode characters.
     #
     # Will allow (for example) "", "-", "9", all of which are invalid domain names, as well as domain
     # names that are longer than 63 characters.
@@ -149,7 +149,8 @@ exit_if_suspicious_domain_name() {
         fi
 
         if [ "$ch" = "." ] \
-        || [ "$ch" = "-" ] ; then # . or -
+        || [ "$ch" = "-" ] \
+        || [ "$ch" = " " ] ; then # . or - or space
             continue;
         fi
 